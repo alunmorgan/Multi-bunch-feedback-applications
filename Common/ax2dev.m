6a5946cc6e4e7c6f17648c98a0a3b3c20bdcf508
Alun Morgan <alun.morgan@diamond.ac.uk>
2018-08-29 14:38:40 +0100

    Updated to new hardware names.

--- Common/ax2dev.m
+++ Common/ax2dev.m
@@ -6,9 +6,9 @@ function dev=ax2dev(axis)
 % axis 1|2|3 for X|Y|Z
 % example dev=ax2dev(axis)
 if axis == 1
-    dev = 'SR23C-DI-TMBF-04:X';
+    dev = 'SR23C-DI-TMBF-01:X';
 elseif axis == 2
-    dev = 'SR23C-DI-TMBF-04:Y';
+    dev = 'SR23C-DI-TMBF-01:Y';
 elseif axis == 3
     dev = 'SR23C-DI-LMBF-01:IQ';
 else
