9272343a24885be59f2c9b729520af8c7f36bea3
Alun Morgan <alun.morgan@diamond.ac.uk>
2018-08-29 09:13:55 +0100

    Modifying ax2dev in order to use the new device names.

--- Common/ax2dev.m
+++ Common/ax2dev.m
@@ -3,10 +3,14 @@ function dev=ax2dev(axis)
 % this functions is called from all others which allow specification of
 % axis
 % change to suit your needs
-% axis 1|2 for X|Y
+% axis 1|2|3 for X|Y|Z
 % example dev=ax2dev(axis)
-if axis <4
-    dev = ['SR23C-DI-TMBF-0' num2str(axis)];
+if axis == 1
+    dev = 'SR23C-DI-TMBF-04:X';
+elseif axis == 2
+    dev = 'SR23C-DI-TMBF-04:Y';
+elseif axis == 3
+    dev = 'SR23C-DI-LMBF-01:IQ';
 else
     dev='TS-DI-TMBF-01';
 end
