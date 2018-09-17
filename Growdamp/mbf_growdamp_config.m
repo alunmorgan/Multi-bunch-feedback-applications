dbdee9aca34c3aad799ff93bee4aea10081ad931
Alun Morgan <alun.morgan@diamond.ac.uk>
2018-09-17 11:51:46 +0100

    Commented out unused settings. To be checked on real hardware.

--- Growdamp/mbf_growdamp_config.m
+++ Growdamp/mbf_growdamp_config.m
@@ -15,7 +15,7 @@ if strcmpi(mbf_axis, 'x')
     settings.durations = [250, 250, 250, 500]; %[excitation, passive damping, active damping, gap]
     settings.dwell = 1;
     settings.tune_sweep_range = [80.00500, 80.49500];
-    settings.det_input = 'FIR';
+%     settings.det_input = 'FIR';
 elseif strcmpi(mbf_axis, 'y')
     settings.axis_label = 'y';
     settings.axis_number = 2;
@@ -24,7 +24,7 @@ elseif strcmpi(mbf_axis, 'y')
     settings.durations = [250, 250, 250, 500]; %[excitation, passive damping, active damping,gap]
     settings.dwell = 1;
     settings.tune_sweep_range = [80.00500, 80.49500];
-    settings.det_input = 'FIR';
+%     settings.det_input = 'FIR';
 elseif strcmpi(mbf_axis, 's')
     settings.axis_label = 's';
     settings.axis_number = 3;
@@ -33,5 +33,5 @@ elseif strcmpi(mbf_axis, 's')
     settings.durations = [10, 10, 50, 100]; %[excitation, passive damping, active damping, gap]
     settings.dwell = 480;
     settings.tune_sweep_range = [80.00220, 80.00520];
-    settings.det_input = 'FIR';
+%     settings.det_input = 'FIR';
 end %if
