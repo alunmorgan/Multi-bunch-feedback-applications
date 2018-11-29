e93efcff965c95a6d9166ea011b8d07df61b4eef
Alun Morgan <alun.morgan@diamond.ac.uk>
2018-11-29 16:39:51 +0000

    Refactoring mbf_spectrum.

---
+++ Spectrum/mbf_spectrum_plotting.m
@@ -0,0 +1,66 @@
+function mbf_spectrum_plotting(data)
+% Plots the data saved and analysed by the earlier mbf_spectrum functions.
+% Args:
+%         data(structure): analysed data.
+%
+% Example: mbf_spectrum_plotting(data)
+
+graph_handles(1) = figure;
+ax1 = subplot('position',[.02 .35 .7 .6]);
+imagesc(data.bunch_axis,...
+    data.tune_axis,...
+    log10(data.bunch_data),...
+    [-3 0]+log10(max(max(data.bunch_data))));
+set(ax1,'YAxisLocation','right');
+set(ax1,'YDir','normal')
+set(ax1, 'XTick', [])
+set(ax1, 'YTick', [])
+title(data.axis_label)
+
+% Frequencies graph
+ax2 = subplot('position',[.72 .35 .18 .6]);
+plot(data.bunch_tune ,data.tune_axis);
+set(ax2,'YAxisLocation','right');
+set(ax2,'XAxisLocation','top')
+set(ax2, 'XTick', [])
+ylabel('Fractional tune')
+axis tight
+
+% bunches graph
+[~,pi] = max(data.mode_tune);
+ax3 = subplot('position',[.02 .11 .7 .24]);
+plot(data.bunch_data(pi,:))
+xlabel(sprintf('Bunches at peak tune %3.3f',data.tune_axis(pi)))
+set(ax3, 'YTick', [])
+axis tight
+
+linkaxes([ax1, ax3], 'x')
+linkaxes([ax1, ax2], 'y')
+
+graph_handles(2) = figure;
+ax4 = subplot('position',[.02 .35 .7 .6]);
+imagesc(data.mode_axis,...
+    data.tune_axis,...
+    log10(data.tune_data(1:end/2,:)),...
+    [-3 0]+log10(max(max(data.tune_data(1:end/2,:)))));
+set(ax4,'YAxisLocation','right');
+set(ax4,'YDir','normal')
+set(ax4, 'XTick', [])
+set(ax4, 'YTick', [])
+
+ax5 = subplot('position',[.72 .35 .18 .6]);
+plot(data.mode_tune,data.tune_axis);
+set(gca,'YAxisLocation','right');
+set(gca,'XAxisLocation','top')
+set(ax5, 'XTick', [])
+ylabel('Fractional tune')
+axis tight
+
+ax6 = subplot('position',[.02 .11 .7 .24]);
+plot(data.mode_axis.',data.tune_data(pi,:))
+xlabel(sprintf('Modes at peak tune %3.3f',data.tune_axis(pi)))
+set(ax6, 'YTick', [])
+axis tight
+
+linkaxes([ax4, ax6], 'x')
+linkaxes([ax4, ax5], 'y')
---
+++ Spectrum/mbf_spectrum_setup.m
@@ -0,0 +1,25 @@
+function mbf_spectrum_setup(mbf_axis)
+% sets up the hardware ready to capture data for a spectrum.
+% Args:
+%       mbf_axis (str): 'x', 'y', 's'. Defines which system you are requesting
+%
+% Example mbf_spectrum_setup('x')
+
+[~, ~, pv_names, trigger_inputs] = mbf_system_config;
+
+% Disarm the sequencer and memory triggers
+mbf_get_then_put([pv_names.hardware_names.(mbf_axis) pv_names.tails.triggers.SEQ.disarm], 1)
+mbf_get_then_put([system_name pv_names.tails.triggers.MEM.disarm], 1)
+
+for trigger_ind = 1:length(trigger_inputs)
+    trigger = trigger_inputs{trigger_ind};
+    mbf_get_then_put([pv_head pv_names.tails.triggers.MEM.(trigger).enable_status], 'Ignore');
+    mbf_get_then_put([pv_head pv_names.tails.triggers.MEM.(trigger).blanking_status], 'All');
+end %for
+% Set the trigger to one shot
+mbf_get_then_put([pv_head pv_names.tails.triggers.MEM.mode], 'One Shot');
+% Set the triggering to External only
+lcaPut([pv_head pv_head pv_names.triggers.MEM.('EXT').enable_status], 'Enable')
+
+%  set up the memory buffer to capture ADC data.
+mbf_get_then_put([pv_head, pv_head pv_names.MEM.channel_select], 'ADC0/ADC1')
