142f38a164df4eee7596bd9355c5910f12dea787
Alun Morgan <alun.morgan@diamond.ac.uk>
2018-12-04 10:54:19 +0000

    Fixed incorrect variable name. Completed adjustment to use new mbf_read_mem function.

--- Bunch motion/mbf_bunch_motion_capture.m
+++ Bunch motion/mbf_bunch_motion_capture.m
@@ -23,19 +23,21 @@ pv_head_T = pv_names.hardware_names.T;
 pv_head_L = pv_names.hardware_names.L;
 
 %Disarm, so that the current settings will be picked up upon arming.
-lcaPut([pv_head_T, pv_names.tails.triggers.mem.disarm], 1)
-lcaPut([pv_head_L, pv_names.tails.triggers.mem.disarm], 1)
+lcaPut([pv_head_T, pv_names.tails.triggers.MEM.disarm], 1)
+lcaPut([pv_head_L, pv_names.tails.triggers.MEM.disarm], 1)
 
 %% Trigger the measurement on all three axes
 % Arming the systems
-mbf_get_then_put({[pv_head_T pv_names.tails.MEM_arm];...
-    [pv_head_L pv_names.tails.MEM_arm]},1);
+mbf_get_then_put({[pv_head_T pv_names.tails.triggers.MEM.arm];...
+    [pv_head_L pv_names.tails.triggers.MEM.arm]},1);
 
 % Triggering the measurement. %%%% SHOULD TRIGGER ON THE NEXT EXTERNAL
 % lcaPut(pv_names.Hardware_trigger, 'On')
 
 % Triggering memory under a lock.
-[bunch_motion.x, bunch_motion.y] = mbf_read_mem(pv_names.hardware_names.T, turn_count,'offset', turn_offset,'channel', -1, 'lock', 60);
+bunch_motion_temp = mbf_read_mem(pv_names.hardware_names.T, turn_count,'offset', turn_offset,'channel', -1, 'lock', 60);
+bunch_motion.x = bunch_motion_temp(:,1);
+bunch_motion.y = bunch_motion_temp(:,2);
 bunch_motion.z = mbf_read_mem(pv_names.hardware_names.L, turn_count, 'offset', turn_offset,'channel', 0, 'lock', 60);
 
 
