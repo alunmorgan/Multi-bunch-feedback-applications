e130b71b3cbb23b78156a7562378ccd684601434
Alun Morgan <alun.morgan@diamond.ac.uk>
2020-11-11 09:43:29 +0000

    fixed the saving to use savemat.

--- python_port/Common/mbf_get_then_put.py
+++ python_port/Common/mbf_get_then_put.py
@@ -1,10 +1,11 @@
-from cothread import caGet, caPut
+from cothread.catools import caGet, caPut
 from Common import mbfSystemConfig
 import os
+from scipy.io import savemat
 
 
 def mbfGetThenPut(pv_name, new_value):
-    """ retrieves the value before the caPut and then
+    """retrieves the value before the caPut and then
         write the new value to the PV.
     Example: mbf_get_then_put(pv_name, new_value)
     """
@@ -15,5 +16,6 @@ def mbfGetThenPut(pv_name, new_value):
     for hse in range(len(pv_name)):
         original_value = caGet(pv_name[hse])
         caPut(pv_name, new_value)
-        save(os.path.join(root_string, 'captured_config', pv_name[hse])),
-            'original_value')
+        savemat(
+            os.path.join((root_string, "captured_config", pv_name[hse])), original_value
+        )
