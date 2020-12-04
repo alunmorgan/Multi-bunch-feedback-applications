ab439ac344180eb30f7b2d1e3cb758d2eb81507f
Alun Morgan <alun.morgan@diamond.ac.uk>
2020-12-04 14:36:40 +0000

    Made the inclusion of the active traces user settable at runtime.

--- Growdamp/mbf_growdamp_plot_summary.m
+++ Growdamp/mbf_growdamp_plot_summary.m
@@ -1,19 +1,22 @@
-function mbf_growdamp_plot_summary(poly_data, frequency_shifts)
+function mbf_growdamp_plot_summary(poly_data, frequency_shifts, outputs)
 % Plots the driven growth rates, and the active and pasive damping rates
 % across all modes.
 %
 % Args:
 %       poly_data (3 by 3 matrix): axis 1 is coupling mode.
 %                                  axis 2 is expermental state,
-%                                  excitation, natural damping, active damping). 
-%                                  axis 3 is damping time, offset and 
+%                                  excitation, natural damping, active damping).
+%                                  axis 3 is damping time, offset and
 %                                  fractional error.
 %       frequency_shifts (list of floats): The frequency shift of each mode.
+%       outputs (str): 'passive' or 'both' Determines if teh active traces are
+%                      included in the graphs
 %
-% Example: mbf_growdamp_plot_summary(poly_data, frequency_shifts)
+% Example: mbf_growdamp_plot_summary(poly_data, frequency_shifts, 'passive')
+
 
 % Getting the desired system setup parameters.
-harmonic_number = length(frequency_shifts);
+harmonic_number = size(frequency_shifts, 1);
 
 x_plt_axis = (0:harmonic_number-1) - harmonic_number/2;
 passive_data = -squeeze(poly_data(:,2,1));
@@ -26,24 +29,35 @@ active_errors(isnan(active_data)) = 0;
 f_shifts = frequency_shifts;
 figure
 ax1 = subplot(3,1,1:2);
-plot(x_plt_axis, circshift(passive_data, -harmonic_number/2, 1), 'b')
+plot(x_plt_axis, circshift(passive_data, -harmonic_number/2, 1), 'b', 'DisplayName', 'Passive')
 hold on
-plot(x_plt_axis, circshift(active_data, -harmonic_number/2, 1), 'g')
-plot(x_plt_axis, zeros(length(x_plt_axis),1), 'r:')
-plot(x_plt_axis, circshift(passive_errors, -harmonic_number/2, 1), 'c*')
-plot(x_plt_axis, circshift(active_errors, -harmonic_number/2, 1), 'm*')
+go1 = plot(x_plt_axis, circshift(passive_errors, -harmonic_number/2, 1), 'c*');
+go1.Annotation.LegendInformation.IconDisplayStyle = 'off';
+if strcmp(outputs, 'both')
+    plot(x_plt_axis, circshift(active_data, -harmonic_number/2, 1), 'g', 'DisplayName', 'Active')
+    go2 = plot(x_plt_axis, circshift(active_errors, -harmonic_number/2, 1), 'm*');
+    go2.Annotation.LegendInformation.IconDisplayStyle = 'off';
+end %if
+go3 = plot(x_plt_axis, zeros(length(x_plt_axis),1), 'r:');
+go3.Annotation.LegendInformation.IconDisplayStyle = 'off';
 hold off
 xlim([x_plt_axis(1) x_plt_axis(end)])
-title('Damping rates for different modes')
+title('MBF growdamp results')
 xlabel('Mode')
 ylabel('Damping rates (1/turns)')
-legend('Passive', 'Active')
+legend
+grid on
 
 ax2 = subplot(3,1,3);
-plot(x_plt_axis, circshift(f_shifts, -harmonic_number/2, 1))
+plot(x_plt_axis, circshift(frequency_shifts(:,1), -harmonic_number/2, 1), 'b')
+if strcmp(outputs, 'both')
+    hold on
+    plot(x_plt_axis, circshift(frequency_shifts(:,2), -harmonic_number/2, 1), 'g')
+    hold off
+end %if
 xlim([x_plt_axis(1) x_plt_axis(end)])
-title('Tune shift from excitation')
 xlabel('Mode')
-ylabel('Difference from excitation tune')
+ylabel({'Difference from';'excitation tune'})
+grid on
 
 linkaxes([ax1, ax2], 'x')
