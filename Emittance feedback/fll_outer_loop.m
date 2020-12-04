21e92f198b30deaa5b7ea04b1384db56cd9bda66
Alun Morgan <alun.morgan@diamond.ac.uk>
2020-12-04 14:35:00 +0000

    Fixed various typos found during live testing.

--- Emittance feedback/fll_outer_loop.m
+++ Emittance feedback/fll_outer_loop.m
@@ -1,13 +1,14 @@
 function fll_outer_loop(name)
 % Monitor the FLL lock state and if unlocked re initialise.
 % name          <EPICS device>:<axis>
-fll_initialisation(mbf_axis)
+fll_initialisation(name)
 
-lcaSetMonitor([name, 'PLL:CTRL:STATUS'])
+%lcaSetMonitor([name, 'PLL:CTRL:STATUS'])
 while 1 == 1
-    lcaNewMonitorWait([name, 'PLL:CTRL:STATUS'])
+%    lcaNewMonitorWait([name, 'PLL:CTRL:STATUS'])
     fll_status = lcaGet([name, 'PLL:CTRL:STATUS']);
     if strcmpi(fll_status, 'Stopped')
-        fll_initialisation(mbf_axis)
+        fll_initialisation(name)
     end %if
+    pause(1)
 end %while
