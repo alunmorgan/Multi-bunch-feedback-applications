9a400c964f22619bebf82dbeac355193f1e5c26f
Alun Morgan <alun.morgan@diamond.ac.uk>
2021-02-09 17:29:19 +0000

    Changed the input to use the parser. This allows sensible defaults to be set on inputs. Changed option to allow outputs to be either passive, active or both.

--- Growdamp/mbf_growdamp_plot_summary.m
+++ Growdamp/mbf_growdamp_plot_summary.m
@@ -1,4 +1,4 @@
-function mbf_growdamp_plot_summary(poly_data, frequency_shifts, outputs)
+function mbf_growdamp_plot_summary(poly_data, frequency_shifts, varargin)
 % Plots the driven growth rates, and the active and pasive damping rates
 % across all modes.
 %
@@ -9,12 +9,19 @@ function mbf_growdamp_plot_summary(poly_data, frequency_shifts, outputs)
 %                                  axis 3 is damping time, offset and
 %                                  fractional error.
 %       frequency_shifts (list of floats): The frequency shift of each mode.
-%       outputs (str): 'passive' or 'both' Determines if teh active traces are
+%       outputs (str): 'passive' 'active', or 'both' Determines which traces are
 %                      included in the graphs
 %
 % Example: mbf_growdamp_plot_summary(poly_data, frequency_shifts, 'passive')
 
-
+p = inputParser;
+p.StructExpand = false;
+p.CaseSensitive = false;
+valid_string = @(x) ischar(x);
+addRequired(p, 'poly_data');
+addRequired(p, 'frequency_shifts');
+addParameter(p, 'outputs', 'passive', valid_string);
+parse(p, mbf_axis, tune, varargin{:});
 % Getting the desired system setup parameters.
 harmonic_number = size(frequency_shifts, 1);
 
@@ -26,14 +33,15 @@ passive_errors(isnan(passive_data)) = 0;
 active_errors = NaN(length(active_data),1);
 active_errors(isnan(active_data)) = 0;
 
-f_shifts = frequency_shifts;
 figure
 ax1 = subplot(3,1,1:2);
-plot(x_plt_axis, circshift(passive_data, -harmonic_number/2, 1), 'b', 'DisplayName', 'Passive')
 hold on
-go1 = plot(x_plt_axis, circshift(passive_errors, -harmonic_number/2, 1), 'c*');
-go1.Annotation.LegendInformation.IconDisplayStyle = 'off';
-if strcmp(outputs, 'both')
+if strcmpi(outputs, 'passive') || strcmpi(outputs, 'both')
+    plot(x_plt_axis, circshift(passive_data, -harmonic_number/2, 1), 'b', 'DisplayName', 'Passive')
+    go1 = plot(x_plt_axis, circshift(passive_errors, -harmonic_number/2, 1), 'c*');
+    go1.Annotation.LegendInformation.IconDisplayStyle = 'off';
+end %if
+if strcmpi(outputs, 'active') || strcmpi(outputs, 'both')
     plot(x_plt_axis, circshift(active_data, -harmonic_number/2, 1), 'g', 'DisplayName', 'Active')
     go2 = plot(x_plt_axis, circshift(active_errors, -harmonic_number/2, 1), 'm*');
     go2.Annotation.LegendInformation.IconDisplayStyle = 'off';
@@ -49,15 +57,18 @@ legend
 grid on
 
 ax2 = subplot(3,1,3);
-plot(x_plt_axis, circshift(frequency_shifts(:,1), -harmonic_number/2, 1), 'b')
-if strcmp(outputs, 'both')
     hold on
-    plot(x_plt_axis, circshift(frequency_shifts(:,2), -harmonic_number/2, 1), 'g')
-    hold off
+if strcmpi(outputs, 'passive') || strcmpi(outputs, 'both')
+    plot(x_plt_axis, circshift(frequency_shifts(:,1), -harmonic_number/2, 1), 'b', 'DisplayName', 'Passive')
 end %if
+if strcmpi(outputs, 'active') || strcmpi(outputs, 'both')
+    plot(x_plt_axis, circshift(frequency_shifts(:,2), -harmonic_number/2, 1), 'g', 'DisplayName', 'Active')
+end %if
+    hold off
 xlim([x_plt_axis(1) x_plt_axis(end)])
 xlabel('Mode')
 ylabel({'Difference from';'excitation tune'})
+legend
 grid on
 
 linkaxes([ax1, ax2], 'x')
