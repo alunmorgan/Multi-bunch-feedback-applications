c84d606ba91fd84ca20054e23b997fea449fe9ac
Alun Morgan <alun.morgan@diamond.ac.uk>
2021-06-08 15:53:16 +0100

    Fixed errors in bash command construction. Forcing axis valus to uppercase which simplifies comparison code.

--- Common/setup_operational_mode.m
+++ Common/setup_operational_mode.m
@@ -4,22 +4,19 @@ function setup_operational_mode(mbf_axis, operational_mode)
 %   Args:
 %       mbf_axis (char): x, y or  s
 %       operational_mode (char): 'TuneOnly', 'TuneSpecial' or 'Feedback'
-
-expected_axis_values = ["x", "X", "y", "Y" "s", "S"];
-axis = validatestring(mbf_axis, expected_axis_values);
+mbf_axis = upper(mbf_axis);
+expected_axis_values = ["X", "Y", "S"];
+mbf_axis = validatestring(mbf_axis, expected_axis_values);
 
 expected_operational_modes = ["TuneOnly", "TuneSpecial", "Feedback"];
 action = validatestring(operational_mode, expected_operational_modes);
 
-if strcmpi(axis, 'x')
-    axis = "X";
-    device = "SR23C-DI-TMBF-01";
-elseif strcmpi(axis, 'y')
-    axis = "Y";
-    device = "SR23C-DI-TMBF-01";
-elseif strcmpi(axis, 's')
-    axis = "IQ";
-    device = "SR23C-DI-LMBF-01";
+if strcmp(mbf_axis, "X")
+    device = "SR23C-DI-TMBF-01:X";
+elseif strcmp(mbf_axis, "Y")
+    device = "SR23C-DI-TMBF-01:Y";
+elseif strcmp(mbf_axis, "S")
+    device = "SR23C-DI-LMBF-01:IQ";
 end %if
 
 if strcmp(action, "TuneOnly")
@@ -30,9 +27,15 @@ elseif strcmp(action, "Feedback")
     action = "FB";
 end %if
 
-% construct the bash command
-bash_command1 = 'gui_dir="$(configure-ioc s -p DI-MBF-gui-dir)"';
-bash_command2 = "$gui_dir/epics/opi/scripts/mbf-run-command 'description' mbf-setup-tune " + device + ":" + axis + ' ' + action;
+% Pick up the base directory
+[rc, gui_dir] = system("configure-ioc s -p DI-MBF-gui-dir");
+assert(rc == 0, "Error running `configure-ioc`");
+gui_dir = strip(gui_dir, "right");  % Get rid of trailing newline
 
-% status1 = system(bash_command1);
-% status2 = system(bash_command2);
+% construct the bash command
+description = "Setting MBF defaults";
+bash_command = sprintf("%s/%s '%s' mbf-setup-tune %s %s", ...
+    gui_dir, "/epics/opi/scripts/mbf-run-command", ...
+    description, device, action);
+rc = system(bash_command);
+assert(rc == 0, 'Error running mbf-setup-tune');
