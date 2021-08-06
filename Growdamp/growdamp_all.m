6b9922755deac9c66dbfd8a776248195e4873642
Alun Morgan <alun.morgan@diamond.ac.uk>
2021-08-06 10:35:18 +0100

    Changed the mode to be Feedback and tune rather than tune only as this gives better initial state before the measurment if there is an instability present. Removed the initial setting of mode as this caused beam kick out in some circumstances. The code now assumes that the system is in a good state. In future the mode switch will be replaced by a tune tracking setup.

--- Growdamp/growdamp_all.m
+++ Growdamp/growdamp_all.m
@@ -4,23 +4,29 @@ function growdamp_all
 
 mbf_tools
 
-% Programatiaclly press the tune only button on each system
+% Programatiaclly press the Feedback and tune button on each system
 % then get the tunes
-setup_operational_mode("x", "TuneOnly")
+
+% setup_operational_mode("x", "Feedback")
+% lcaPut('SR23C-DI-TMBF-01:X:FIR:GAIN_S', '0dB')
+% setup_operational_mode("y", "Feedback")
+% lcaPut('SR23C-DI-TMBF-01:Y:FIR:GAIN_S', '0dB')
+% setup_operational_mode("s", "Feedback")
+% lcaPut('SR23C-DI-LMBF-01:IQ:FIR:GAIN_S', '0dB')
+
 x_tune = lcaGet('SR23C-DI-TMBF-01:X:TUNE:TUNE');
 if isnan(x_tune)
     x_tune = lcaGet('SR23C-DI-TMBF-01:X:TUNE:TUNE');
 end %if
-setup_operational_mode("y", "TuneOnly")
 y_tune =  lcaGet('SR23C-DI-TMBF-01:Y:TUNE:TUNE');
 if isnan(y_tune)
     y_tune = lcaGet('SR23C-DI-TMBF-01:Y:TUNE:TUNE');
 end %if
-setup_operational_mode("s", "TuneOnly")
 s_tune = lcaGet('SR23C-DI-LMBF-01:IQ:TUNE:TUNE');
 if isnan(s_tune)
     s_tune = lcaGet('SR23C-DI-LMBF-01:IQ:TUNE:TUNE');
 end %if
+
 
 mbf_growdamp_setup('x', x_tune)
 growdamp_x = mbf_growdamp_capture('x');
@@ -32,9 +38,7 @@ mbf_growdamp_setup('s', s_tune)
 growdamp_s = mbf_growdamp_capture('s');
 
 % Programatiaclly press the tune only button on each system
-setup_operational_mode("x", "TuneOnly")
-setup_operational_mode("y", "TuneOnly")
-setup_operational_mode("s", "TuneOnly")
+
 
 [poly_data_x, frequency_shifts_x] = mbf_growdamp_analysis(growdamp_x);
 mbf_growdamp_plot_summary(poly_data_x, frequency_shifts_x, ...
@@ -47,4 +51,9 @@ mbf_growdamp_plot_summary(poly_data_y, frequency_shifts_y, ...
 [poly_data_s, frequency_shifts_s] = mbf_growdamp_analysis(growdamp_s);
 mbf_growdamp_plot_summary(poly_data_s, frequency_shifts_s,...
     'outputs', 'both', 'axis', 's')
-
+setup_operational_mode("x", "Feedback")
+lcaPut('SR23C-DI-TMBF-01:X:FIR:GAIN_S', '0dB')
+setup_operational_mode("y", "Feedback")
+lcaPut('SR23C-DI-TMBF-01:Y:FIR:GAIN_S', '0dB')
+setup_operational_mode("s", "Feedback")
+lcaPut('SR23C-DI-LMBF-01:IQ:FIR:GAIN_S', '0dB')
