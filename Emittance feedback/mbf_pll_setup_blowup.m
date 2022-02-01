54acf599c33dec204d40c5496d929b2f95c4c75d
Alun Morgan <alun.morgan@diamond.ac.uk>
2022-02-01 13:43:52 +0000

    Making monitor bunch selection and guard bunches input parameters (with defaults) rather than set variables.

--- Emittance feedback/mbf_pll_setup_blowup.m
+++ Emittance feedback/mbf_pll_setup_blowup.m
@@ -1,12 +1,37 @@
 function mbf_pll_setup_blowup(varargin)
+% Sets up the Multibunch feedback system to run a frequency locked loop on a
+% single bunch in each plane. The use the tracked frequency to run an
+% oscillator on the sideband of the tune.
+%   Args: (All optional)
+%       excitation_x(float): The power of the oscillator in the x axis (in dB).
+%       excitation_y(float): The power of the oscillator in the y axis (in dB).
+%       fll_monitor_bunches_x(vector of floats): The bunches to be used by the
+%                                                FLL for tune tracking (x axis).
+%       fll_monitor_bunches_y(vector of floats): The bunches to be used by the
+%                                                FLL for tune tracking (y axis).
+%       guardbunches_x(int): The number of bunches around the FLL monitor bunches
+%                            for which the feedback is turned off. 
+%                            This is to reduce distortion of the monitored signal
+%       guardbunches_y(int): The number of bunches around the FLL monitor bunches
+%                            for which the feedback is turned off. 
+%                            This is to reduce distortion of the monitored signal
 
 default_excitation_x = -60;
 default_excitation_y = -60;
+default_fll_monitor_bunches_x=400;
+default_fll_monitor_bunches_y=300;
+default_guardbunches_x = 10;
+default_guardbunches_y = 10;
 
 validScalarNum = @(x) isnumeric(x) && isscalar(x);
+validNum = @(x) isnumeric(x) && isscalar(x);
 p = inputParser;
 addParameter(p, 'excitation_x', default_excitation_x, validScalarNum);
 addParameter(p, 'excitation_y', default_excitation_y, validScalarNum);
+addParameter(p, 'fll_monitor_bunches_x', default_fll_monitor_bunches_x, validNum);
+addParameter(p, 'fll_monitor_bunches_y', default_fll_monitor_bunches_y, validNum);
+addParameter(p, 'guardbunches_x', default_guardbunches_x, validScalarNum);
+addParameter(p, 'guardbunches_y', default_guardbunches_y, validScalarNum);
 parse(p,varargin{:});
 
 namex = mbf_axis_to_name('x');
@@ -14,14 +39,10 @@ namey = mbf_axis_to_name('y');
 
 % initialise PLL on two different arbitrary (hopefully these are filled) 
 % bunches on both planes.
-fllbunches_x=400;
-fllbunches_y=300;
-guardbunches_x = 10;
-guardbunches_y = 10;
-% mbf_pll_start(namex,pllbunches_x,10)
-% mbf_pll_start(namey,pllbunches_y,10)
-mbf_fll_start('x', 'fllbunches',fllbunches_x, 'guardbunches',guardbunches_x)
-mbf_fll_start('y', 'fllbunches',fllbunches_y, 'guardbunches',guardbunches_y)
+mbf_fll_start('x', 'fllbunches',p.Results.fll_monitor_bunches_x,...
+    'guardbunches',p.Results.guardbunches_x)
+mbf_fll_start('y', 'fllbunches',p.Results.fll_monitor_bunches_y,...
+    'guardbunches',p.Results.guardbunches_y)
 
 % Grab frequencies of left sideband from swept tune fitter
 [x_tune, y_tune, ~] = get_all_tunes;
