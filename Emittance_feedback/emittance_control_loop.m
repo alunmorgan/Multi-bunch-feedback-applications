2042c35d56f00851f14a8c3246560c3f2c38d13c
Alun Morgan <alun.morgan@diamond.ac.uk>
2022-09-23 12:24:05 +0100

    Changed the logic to make slew rate be in terms of emittance. Added a switch to the mean value once the instantanious value is close enough to the mean. This is to reduce the jitter around the set point.

--- Emittance_feedback/emittance_control_loop.m
+++ Emittance_feedback/emittance_control_loop.m
@@ -15,12 +15,15 @@ elseif strcmp(selected_axis, 'Y')
 else
     error('Invalid axis selected for emittance control loop')
 end %if
-slew_rate_limit = 0.2;
-low_limit = 0.00098; %~-60dB
-high_limit = 0.0625; %~-20dB
-start_level = 0.004; %~-46dB
+slew_rate_limit = 0.2; % in pm rad for Y and nm rad for X
+fraction_to_apply = 0.075;
+low_power_limit = 0.00098; %~-60dB
+high_power_limit = 0.0625; %~-20dB
+start_power_level = 0.004; %~-46dB
 output_lim = 1;
-lcaPut(['SR23C-DI-TMBF-01:',selected_axis,':NCO2:GAIN_SCALAR_S'], start_level);
+lcaPut(['SR23C-DI-TMBF-01:',selected_axis,':NCO2:GAIN_SCALAR_S'], start_power_level);
+emit_old = lcaGet(['SR-DI-EMIT-01:',em_axis,'EMIT']);
+
 while true
     % The ~=0 accounts for the -1 when topup is turned off as well as the usual countdown when it is on.
     if lcaGet('SR-CS-FILL-01:COUNTDOWN') ~= 0  && lcaGet('SR-DI-DCCT-01:SIGNAL') > 10 && strcmp(lcaGet('SR-DI-EMIT-01:STATUS'), 'Successful')
@@ -45,25 +48,35 @@ while true
         if ~strcmp(lcaGet(['SR23C-DI-TMBF-01:',selected_axis,':NCO2:ENABLE_S']), 'On')
             error('Excitation manually terminated')
         end %if
-        
+
         tune = lcaGet(['SR23C-DI-TMBF-01:',selected_axis,':TUNE:CENTRE:TUNE']);
-        emit=lcaGet(['SR-DI-EMIT-01:',em_axis,'EMIT_MEAN']);
+        emit = lcaGet(['SR-DI-EMIT-01:',em_axis,'EMIT']);
+        emit_mean = lcaGet(['SR-DI-EMIT-01:',em_axis,'EMIT_MEAN']);
         power=lcaGet(['SR23C-DI-TMBF-01:',selected_axis,':NCO2:GAIN_SCALAR_S']);
-        
+
         if isnan(tune)
             % pause if tune value is invalid.
             pause(1)
-%             % Too much excitation causes the tune value to be lost. The solution is
-%         % to reduce the exciation value
-%             lcaPut(['SR23C-DI-TMBF-01:',selected_axis,':NCO2:GAIN_SCALAR_S'],power-0.001);
         else
-            emit_error= 1E-3 * log10(emit / emittance_target);
+            %             emit_error= 1E-3 * log10(emit / emittance_target);
+            %             if abs(emit_error) > slew_rate_limit
+            %                 emit_error = sign(emit_error) * slew_rate_limit;
+            %             end %if
+            %             if power - emit_error > low_power_limit && power - emit_error < high_power_limit
+            %                 lcaPut(['SR23C-DI-TMBF-01:',selected_axis,':NCO2:GAIN_SCALAR_S'], power - emit_error);
+            %             end %if
+            if abs(emit - emit_mean) <0.01
+                emit = emit_mean;
+            end %if
+            emit_error = emittance_target - emit;
             if abs(emit_error) > slew_rate_limit
                 emit_error = sign(emit_error) * slew_rate_limit;
             end %if
-            if power - emit_error > low_limit && power - emit_error < high_limit
-                lcaPut(['SR23C-DI-TMBF-01:',selected_axis,':NCO2:GAIN_SCALAR_S'], power - emit_error);
+            power_error = sign(emit_error) * 1E-3 * log10(abs(emit_error)) * fraction_to_apply;
+            if power - power_error > low_power_limit && power - power_error < high_power_limit
+                lcaPut(['SR23C-DI-TMBF-01:',selected_axis,':NCO2:GAIN_SCALAR_S'], power - power_error);
             end %if
+            emit_old = emit;
         end %if
         pause(0.5)
     else
