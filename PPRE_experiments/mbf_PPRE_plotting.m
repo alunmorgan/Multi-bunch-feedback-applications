45fc2709428e1d8de049f6baef42971a96b410dd
Alun Morgan <alun.morgan@diamond.ac.uk>
2022-11-09 12:02:22 +0000

    Adding basic plotting for scans.

--- PPRE_experiments/mbf_PPRE_plotting.m
+++ PPRE_experiments/mbf_PPRE_plotting.m
@@ -1,4 +1,26 @@
-function mbf_PPRE_plotting(PPRE_data, PPRE_pp)
+function mbf_PPRE_plotting(PPRE_pp)
+
+tiledlayout('flow')
+nexttile
+plot(PPRE_pp.scan_axis, PPRE_pp.emittance_x)
+title('Horizontal emittance')
+xlabel(PPRE_pp.scan_label)
+nexttile
+plot(PPRE_pp.scan_axis, PPRE_pp.emittance_y)
+title('Vertical emittance')
+xlabel(PPRE_pp.scan_label)
+nexttile
+plot(PPRE_pp.scan_axis, PPRE_pp.beam_sizes_P1)
+title('Pinhole 1 vertical beamsize')
+xlabel(PPRE_pp.scan_label)
+nexttile
+plot(PPRE_pp.scan_axis, PPRE_pp.beam_sizes_P2)
+title('Pinhole 2 vertical beamsize')
+xlabel(PPRE_pp.scan_label)
+nexttile
+plot(PPRE_pp.scan_axis, PPRE_pp.beam_sizes_P3)
+title('Pinhole 3 vertical beamsize')
+xlabel(PPRE_pp.scan_label)
 
 
 
--- PPRE_experiments/mbf_PPRE_postprocessing.m
+++ PPRE_experiments/mbf_PPRE_postprocessing.m
@@ -1,6 +1,23 @@
 function data_out = mbf_PPRE_postprocessing(PPRE_data)
 
+if length(PPRE_data.excitation_gain) == length(PPRE_data.scan)
+    data_out.scan_axis = PPRE_data.excitation_gain;
+    data_out.scan_label = 'Gain';
+elseif length(PPRE_data.excitation_frequency) == length(PPRE_data.scan)
+    data_out.scan_axis = PPRE_data.excitation_frequency;
+    data_out.scan_label = 'Frequency';
+elseif length(PPRE_data.harmonic) == length(PPRE_data.scan)
+    data_out.scan_axis = PPRE_data.harmonic;
+    data_out.scan_label = 'Harmonic';
+else
+    data_out.scan_axis = 1:length(PPRE_data.scan);
+    data_out.scan_label = 'Undefined';
+end %if
+
 for i = 1:length(PPRE_data.scan)
-    data_out.emittance_x(i) = PPRE_data.scan{i}.emittance.hemit;
-    data_out.emittance_y(i) = PPRE_data.scan{i}.emittance.veimt;
+    data_out.emittance_x(i) = PPRE_data.scan{i}.emittance{1}.hemit;
+    data_out.emittance_y(i) = PPRE_data.scan{i}.emittance{1}.veimt;
+    data_out.beam_sizes_P1(i) = PPRE_data.scan{i}.beam_sizes{1}.P1_sigy;
+    data_out.beam_sizes_P1(i) = PPRE_data.scan{i}.beam_sizes{1}.P2_sigy;
+    data_out.beam_sizes_P1(i) = PPRE_data.scan{i}.beam_sizes{1}.P3_sigy;
 end %for
