55a73e6b43344951231ab5bd4ca4c777c98b5ab1
Alun Morgan <alun.morgan@diamond.ac.uk>
2022-12-06 17:39:46 +0000

    Added try catch statements to increase overall robustness.

--- mbf_startup_tests.m
+++ mbf_startup_tests.m
@@ -1,13 +1,51 @@
 function mbf_startup_tests
+% Runs all the regular MBF tests
+%
+% Inject 30mA 900 bunches into the machine before running the tests.
+try
+    modscan_all('x', 'plotting', 'no')
+catch
+    disp('Problem with Modescan in X axis')
+end %try
+try
+    modscan_all('y', 'plotting', 'no')
+catch
+    disp('Problem with Modescan in Y axis')
+end %try
+try
+    modscan_all('s', 'plotting', 'no')
+catch
+    disp('Problem with Modescan in S axis')
+end %try
 
-modscan_all('x', 'plotting', 'no')
-modscan_all('y', 'plotting', 'no')
-modscan_all('s', 'plotting', 'no')
+try
+    growdamp_all('x',  'plotting', 'no')
+catch
+    disp('Problem with Growdamp in X axis')
+end %try
+try
+    growdamp_all('y',  'plotting', 'no')
+catch
+    disp('Problem with Growdamp in Y axis')
+end %try
+try
+    growdamp_all('s',  'plotting', 'no')
+catch
+    disp('Problem with Growdamp in S axis')
+end %try
 
-growdamp_all('x',  'plotting', 'no')
-growdamp_all('y',  'plotting', 'no')
-growdamp_all('s',  'plotting', 'no')
-
-mbf_spectrum_all('x',  'plotting', 'no')
-mbf_spectrum_all('y',  'plotting', 'no')
-mbf_spectrum_all('s',  'plotting', 'no')
+try
+    mbf_spectrum_all('x',  'plotting', 'no')
+catch
+    disp('Problem with Spectrum in X axis')
+end %try
+try
+    mbf_spectrum_all('y',  'plotting', 'no')
+catch
+    disp('Problem with Spectrum in Y axis')
+end %try
+try
+    mbf_spectrum_all('s',  'plotting', 'no')
+catch
+    disp('Problem with Spectrum in S axis')
+end %try
