f3024380a31b14dc1bc232c7442a435a77e02b42
Alun Morgan <alun.morgan@diamond.ac.uk>
2022-12-09 14:32:51 +0000

    Added descriptions of IO. restructured the code to clearly identify magnitude.

--- Growdamp/mbf_growdamp_basic_fitting.m
+++ Growdamp/mbf_growdamp_basic_fitting.m
@@ -1,9 +1,22 @@
 function [s, delta, p] = mbf_growdamp_basic_fitting(data, debug)
-
+%
+% Args:
+%       data (vector of floats): The data.
+%       debug(int): if 1 then outputs graphs of individual modes to allow
+%                                    selection of appropriate overrides.
+%
+%   Returns:
+%       s(vector of floats): coefficients of the polynomial fit of the
+%                            magnitude of data
+%       delta(float): deviation of the magnitude data to the 
+%                     fit of the magnitude data.
+%       p(vector of floats): coefficients of the polynomial fit of the 
+%                            phase of data
 x_ax = 1:length(data);
-s = polyfit(x_ax,log(abs(data)),1);
+mag = log(abs(data));
+s = polyfit(x_ax, mag, 1);
 c = polyval(s,x_ax);
-delta = mean(abs(c - log(abs(data)))./c);
+delta = mean(abs(c - mag)./c);
 temp = unwrap(angle(data)) / (2*pi);
 p = polyfit(x_ax,temp,1);
 if debug==1
