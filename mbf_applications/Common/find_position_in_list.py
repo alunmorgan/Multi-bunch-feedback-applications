fb2dd86aedceca4eaeaabb8ded99695fc30d6f6e
Alun Morgan <alun.morgan@diamond.ac.uk>
2023-04-18 17:21:15 +0100

    Removing unused python folder. It is out of date and not used.

--- mbf_applications/Common/find_position_in_list.py
+++
@@ -1,9 +0,0 @@
-def findPositionInList(tmp_list, target):
-    # returns the indicies of a target string in a list.
-
-    matched_indexes = []
-    for he in range(len(tmp_list)):
-        if target == tmp_list[he]:
-           matched_indexes.append(he)
-
-    return matched_indexes
--- mbf_applications/Common/find_substring_in_list.py
+++
@@ -1,9 +0,0 @@
-def findSubstringInList(tmp_list, target):
-    # returns the indicies of a target string in a list.
-
-    matched_indexes = []
-    for he in range(len(tmp_list)):
-        if target in tmp_list[he]:
-            matched_indexes.append(he)
-
-    return matched_indexes
