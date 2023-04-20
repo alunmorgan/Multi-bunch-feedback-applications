df923e3980ac54ca495b2ffc23212a8f87b219ea
Alun Morgan <alun.morgan@diamond.ac.uk>
2023-04-20 16:54:57 +0100

    Adding modescan.

--- mbf_data_archive/update_all_indexes.m
+++ mbf_data_archive/update_all_indexes.m
@@ -13,5 +13,8 @@ mbf_make_index('Growdamp', 'y')
 mbf_make_index('Growdamp', 's')
 mbf_make_index('fll_phase_scan', 'x')
 mbf_make_index('fll_phase_scan', 'y')
+mbf_make_index('Modescan', 'x')
+mbf_make_index('Modescan', 'y')
+mbf_make_index('Modescan', 's')
 
 
