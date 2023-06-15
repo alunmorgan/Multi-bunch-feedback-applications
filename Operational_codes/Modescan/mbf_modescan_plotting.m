616918b0d014d892fb67c3fb1697109d2ff5491c
Alun Morgan <alun.morgan@diamond.ac.uk>
2023-06-15 16:14:45 +0100

    Adding limits to the phase graph so that it is easier to see the target area.

--- Operational_codes/Modescan/mbf_modescan_plotting.m
+++ Operational_codes/Modescan/mbf_modescan_plotting.m
@@ -17,8 +17,17 @@ title(['Amplitude (', modescan.ax_label, ' ', num2str(modescan.time(3)),...
     num2str(modescan.time(5)), ')'])
 xlabel('Modes')
 xlim([1,modescan.harmonic_number])
+grid on
 subplot(2,1,2)
-plot(data_phase(1:modescan.harmonic_number))
+plot(data_phase(1:modescan.harmonic_number), 'DisplayName', 'data', 'LineWidth',2)
+p = polyfit(1:modescan.harmonic_number, data_phase(1:modescan.harmonic_number), 1);
+y = polyval(p,1:modescan.harmonic_number);
+hold on 
+plot(1:modescan.harmonic_number, ones(modescan.harmonic_number,1)*-90, ':r', 'DisplayName','Limits', 'LineWidth',2)
+plot(1:modescan.harmonic_number, ones(modescan.harmonic_number,1)*90, ':r', 'LineWidth',2, 'HandleVisibility','off')
+hold off
 title('Phase (deg)')
 xlabel('Modes')
 xlim([1, modescan.harmonic_number])
+legend
+grid on
