6214c31e801f9a53f9f92f97fd6d8fc102a81788
Alun Morgan <alun.morgan@diamond.ac.uk>
2023-07-06 10:50:28 +0100

    Made inputs overridable at runtime. Added comments to make it clearer as to what steps were being taken and why. refactored the code to make the processing steps more distinct.

--- Developmental_codes/Emittance_feedback/emittance_control_loop.m
+++ Developmental_codes/Emittance_feedback/emittance_control_loop.m
@@ -1,4 +1,4 @@
-function emittance_control_loop(selected_axis, emittance_target)
+function emittance_control_loop(selected_axis, emittance_target, varargin)
 % This changes the gain of the excitation for the selected axis in order to
 % reach the requested emittance target.
 % This presumes that mbf_emittance_setup has been run beforehand.
@@ -15,18 +15,42 @@ elseif strcmp(selected_axis, 'Y')
 else
     error('Invalid axis selected for emittance control loop')
 end %if
-slew_rate_limit = 0.2; % in pm rad for Y and nm rad for X
-fraction_to_apply = 0.075;
-low_power_limit = 0.00098; %~-60dB
-high_power_limit = 0.0625; %~-20dB
-start_power_level = 0.004; %~-46dB
-output_lim = 1;
-lcaPut(['SR23C-DI-TMBF-01:',selected_axis,':NCO2:GAIN_SCALAR_S'], start_power_level);
-emit_old = lcaGet(['SR-DI-EMIT-01:',em_axis,'EMIT']);
 
+% The time needed to get new data from the underlying hardware.
+hardware_update_time = 0.5; %sec
+
+default_slew_rate_limit = 0.2; % in pm rad for Y and nm rad for X
+default_fraction_to_apply = 0.075;
+default_low_power_limit = 0.00098; %~-60dB
+default_high_power_limit = 0.0625; %~-20dB
+default_start_power_level = 0.004; %~-46dB
+
+p = inputParser;
+p.StructExpand = false;
+p.CaseSensitive = false;
+valid_number = @(x) isnumeric(x);
+addRequired(p, 'selected_axis');
+addRequired(p, 'emittance_target');
+addParameter(p, 'slew_rate_limit', default_slew_rate_limit, valid_number);
+addParameter(p, 'fraction_to_apply', default_fraction_to_apply, valid_number);
+addParameter(p, 'low_power_limit', default_low_power_limit, valid_number);
+addParameter(p, 'high_power_limit', default_high_power_limit, valid_number);
+addParameter(p, 'start_power_level', default_start_power_level, valid_number);
+parse(p, selected_axis, emittance_target, varargin{:});
+
+% start the NCO excitation.
+lcaPut(['SR23C-DI-TMBF-01:',p.Results.selected_axis,':NCO2:GAIN_SCALAR_S'], p.Results.start_power_level);
+
+output_lim = 1;
 while true
-    % The ~=0 accounts for the -1 when topup is turned off as well as the usual countdown when it is on.
+    % The loop holds the existing settings if topup is on
+    % The ~=0 accounts for the -1 when topup is turned off as well as the
+    % usual countdown when it is on.
+    % The loop holds the existing settings if the current drops below 10mA
+    % The loop holds the existing settings if it is unable to get an up to date
+    % emittance reading.
     if lcaGet('SR-CS-FILL-01:COUNTDOWN') ~= 0  && lcaGet('SR-DI-DCCT-01:SIGNAL') > 10 && strcmp(lcaGet('SR-DI-EMIT-01:STATUS'), 'Successful')
+        %% heartbeat code
         if output_lim >100
             fprintf('.\n')
             output_lim = 1;
@@ -34,55 +58,65 @@ while true
             fprintf('.')
             output_lim = output_lim +1;
         end %if
-        % Check the status of the frequency locked loop.
-        error_user = lcaGet(['SR23C-DI-TMBF-01:',selected_axis,':PLL:CTRL:STOP:STOP']);
-        error_detector_overflow = lcaGet(['SR23C-DI-TMBF-01:',selected_axis,':PLL:CTRL:STOP:DET_OVF']);
-        error_offset = lcaGet(['SR23C-DI-TMBF-01:',selected_axis,':PLL:CTRL:STOP:OFFSET_OVF']);
-        error_magnitude = lcaGet(['SR23C-DI-TMBF-01:',selected_axis,':PLL:CTRL:STOP:MAG_ERROR']);
+
+        %% Check the status of the frequency locked loop.
+        error_user = lcaGet(['SR23C-DI-TMBF-01:',p.Results.selected_axis,':PLL:CTRL:STOP:STOP']);
+        error_detector_overflow = lcaGet(['SR23C-DI-TMBF-01:',p.Results.selected_axis,':PLL:CTRL:STOP:DET_OVF']);
+        error_offset = lcaGet(['SR23C-DI-TMBF-01:',p.Results.selected_axis,':PLL:CTRL:STOP:OFFSET_OVF']);
+        error_magnitude = lcaGet(['SR23C-DI-TMBF-01:',p.Results.selected_axis,':PLL:CTRL:STOP:MAG_ERROR']);
         if ~strcmp(error_detector_overflow{1}, 'Ok') ||...
                 ~strcmp(error_offset{1}, 'Ok') ||...
                 ~strcmp(error_magnitude{1}, 'Ok') ||...
                 ~strcmp(error_user{1}, 'Ok')
-            error([selected_axis,' frequency locked loop has stopped']);
+            error([p.Results.selected_axis,' frequency locked loop has stopped']);
         end %if
-        if ~strcmp(lcaGet(['SR23C-DI-TMBF-01:',selected_axis,':NCO2:ENABLE_S']), 'On')
+        if ~strcmp(lcaGet(['SR23C-DI-TMBF-01:',p.Results.selected_axis,':NCO2:ENABLE_S']), 'On')
             error('Excitation manually terminated')
         end %if
 
-        tune = lcaGet(['SR23C-DI-TMBF-01:',selected_axis,':TUNE:CENTRE:TUNE']);
+        %% Get current settings
+        % This pause is to allow the hardware to update so we know we have fresh
+        % data.
+        pause(hardware_update_time)
+        tune = lcaGet(['SR23C-DI-TMBF-01:',p.Results.selected_axis,':TUNE:CENTRE:TUNE']);
         emit = lcaGet(['SR-DI-EMIT-01:',em_axis,'EMIT']);
         emit_mean = lcaGet(['SR-DI-EMIT-01:',em_axis,'EMIT_MEAN']);
-        power=lcaGet(['SR23C-DI-TMBF-01:',selected_axis,':NCO2:GAIN_SCALAR_S']);
-
+        power_input = lcaGet(['SR23C-DI-TMBF-01:',p.Results.selected_axis,':NCO2:GAIN_SCALAR_S']);
         if isnan(tune)
             % pause if tune value is invalid.
-            pause(1)
-        else
-            %             emit_error= 1E-3 * log10(emit / emittance_target);
-            %             if abs(emit_error) > slew_rate_limit
-            %                 emit_error = sign(emit_error) * slew_rate_limit;
-            %             end %if
-            %             if power - emit_error > low_power_limit && power - emit_error < high_power_limit
-            %                 lcaPut(['SR23C-DI-TMBF-01:',selected_axis,':NCO2:GAIN_SCALAR_S'], power - emit_error);
-            %             end %if
-            if abs(emit - emit_mean) <0.01
-                emit = emit_mean;
-            end %if
-            emit_error = emittance_target - emit;
-            if abs(emit_error) > slew_rate_limit
-                emit_error = sign(emit_error) * slew_rate_limit;
-            end %if
-            power_error = sign(emit_error) * 1E-3 * log10(abs(emit_error)) * fraction_to_apply;
-            if power - power_error > low_power_limit && power - power_error < high_power_limit
-                lcaPut(['SR23C-DI-TMBF-01:',selected_axis,':NCO2:GAIN_SCALAR_S'], power - power_error);
-            end %if
-            emit_old = emit;
+            fprintf('\nTune value is invalid.')
+            continue
         end %if
-        pause(0.5)
+
+        %% Main feedback function
+        % if the difference between the instantaneous emittance value and the
+        % mean value is small then use the mean value. This allows faster
+        % initial following using the instantainoues values but more stable
+        % steady state using the mean values.
+        if abs(emit - emit_mean) <0.01
+            emit = emit_mean;
+        end %if
+        % Get the error term
+        emit_error = p.Results.emittance_target - emit;
+        % Apply slew rate limit.
+        if abs(emit_error) > p.Results.slew_rate_limit
+            emit_error = sign(emit_error) * p.Results.slew_rate_limit;
+        end %if
+        % error scaling from emittance to output power.
+        power_error = sign(emit_error) * 1E-3 * log10(abs(emit_error));
+        % fraction to apply
+        power_error = power_error * p.Results.fraction_to_apply;
+        % power monitor
+        power_new = power_input - power_error;
+        % Apply power limits and apply
+        if power_new > p.Results.low_power_limit && power_new < p.Results.high_power_limit
+            lcaPut(['SR23C-DI-TMBF-01:',p.Results.selected_axis,':NCO2:GAIN_SCALAR_S'], power_new);
+        end %if
     else
-        % pause if top up is running or the current is below 10mA or the
-        % beam is injecting.
+        % The machine is not in a state for the loop to run so wait and try
+        % later.
         pause(1)
+        %% heartbeat code
         if output_lim >100
             fprintf('*\n')
             output_lim = 1;
@@ -90,5 +124,6 @@ while true
             fprintf('*')
             output_lim = output_lim +1;
         end %if
-    end %while
-end %function
+
+    end %if
+end %while
