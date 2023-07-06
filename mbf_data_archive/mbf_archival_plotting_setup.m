1018fca56177ad8a28d35af979fb9c8d647d7160
Alun Morgan <alun.morgan@diamond.ac.uk>
2023-07-06 10:54:23 +0100

    Added ability to gracefully deal with missing environment data.

--- mbf_data_archive/mbf_archival_plotting_setup.m
+++ mbf_data_archive/mbf_archival_plotting_setup.m
@@ -1,7 +1,7 @@
 function mbf_archival_plotting_setup(requested_data, times, experimental_setup)
 % Plots the data processed by mbf_growdamp_archival_analysis.
 % Args:
-%      requested_data(structure): data and metatdata.           
+%      requested_data(structure): data and metatdata.
 %      times (numeric vector): Datenums of the datasets.
 %      experimental_setup (structure): The setup parameters for the
 %                                      analysis.
@@ -25,34 +25,38 @@ annotation('textbox', [0 1-0.3 0.3 0.3], 'String', graph_text, 'FitBoxToText', '
 
 ck = 1;
 for hrd = 1:length(ranges_to_display)
-    if strcmp(ranges_to_display{hrd}, 'time')
-        continue
-    else    
-        axes('OuterPosition', [0.01 ck/3 0.2 0.3]);
-    xlabel('Time')
-    title(ranges_to_display{hrd})
-        data_temp = NaN(length(times),1);
-        for hse = 1:length(times)
-            data_temp(hse) = requested_data{hse}.(ranges_to_display{hrd});
-        end %for
-        plot(times, data_temp, 'o:')
-        datetick
-        ylabel(ranges_to_display{hrd})
-        clear data_temp
-        ck = ck +1;
+    if isfield(requested_data{1}, ranges_to_display{hrd})
+        if strcmp(ranges_to_display{hrd}, 'time')
+            continue
+        else
+            axes('OuterPosition', [0.01 ck/3 0.2 0.3]);
+            xlabel('Time')
+            title(ranges_to_display{hrd})
+            data_temp = NaN(length(times),1);
+            for hse = 1:length(times)
+                data_temp(hse) = requested_data{hse}.(ranges_to_display{hrd});
+            end %for
+            plot(times, data_temp, 'o:')
+            datetick
+            ylabel(ranges_to_display{hrd})
+            clear data_temp
+            ck = ck +1;
+        end %if
     end %if
 end %for
 
-axfp = axes('OuterPosition', [0.01 0 0.2 0.3]);
+if isfield(requested_data{1}, 'fill_pattern')
+    axfp = axes('OuterPosition', [0.01 0 0.2 0.3]);
     hold on
-for hkw = 1:length(requested_data)
-    plot(1:harmonic_number, requested_data{hkw}.fill_pattern, 'b')
-end %for
-ylim([0 inf])
-xlim([1 936])
-set(axfp, 'XTick', [])
-title('Fill pattern variation')
-ylabel('Charge (nC)')
+    for hkw = 1:length(requested_data)
+        plot(1:harmonic_number, requested_data{hkw}.fill_pattern, 'b')
+    end %for
+    ylim([0 inf])
+    xlim([1 936])
+    set(axfp, 'XTick', [])
+    title('Fill pattern variation')
+    ylabel('Charge (nC)')
+end %if
 
 % add labels if it is a parameter sweep.
 if nargin == 4 && strcmp(experimental_setup.anal_type, 'parameter_sweep')
