cd4b420f0834a189de980f0b2ee2e9ae5ff9ce22
Alun Morgan <alun.morgan@diamond.ac.uk>
2023-09-13 11:24:30 +0100

    Made output more readable.

--- analyse_all_files.m
+++ analyse_all_files.m
@@ -12,6 +12,8 @@ fullnames = a(c==0);
 roots = regexprep(roots, root, '');
 for jsf = 1:length(d)
     for hes = 1:size(d{jsf},1)
-        fprintf([roots{jsf},' <strong>',names{jsf}, '</strong> ',d{jsf}(hes).message, ' at line ', num2str(d{jsf}(hes).line), '\n'])
+        this_root = regexprep(roots{jsf}, '\\', '\\\');
+        output_message = [this_root,' <strong>',names{jsf}, '</strong> \n',d{jsf}(hes).message, ' at line ', num2str(d{jsf}(hes).line), '\n'];
+        fprintf(output_message)
     end %for
 end %for
