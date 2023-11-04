64ee2001218f58341b4ce06b16b9c92ee62b05b3
Alun Morgan <afdm76@cs01r-cs-pc-05.pri.diamond.ac.uk>
2023-11-04 14:35:08 +0000

    Removed hard coded PVs.

--- Developmental_codes/Tunescan_over_modes/configure_tune_sweep.m
+++ Developmental_codes/Tunescan_over_modes/configure_tune_sweep.m
@@ -4,6 +4,11 @@ function configure_tune_sweep(axis, drive_bunch, fb_on_off, d0flag, d1flag, d2fl
 % drive_bunch = 123;
 % fb_on_off = 1;      % 0 => off, 1 => on
 
+[~, ~, pv_names] = mbf_system_config;
+head = pv_names.hardware_names.(axis);
+bank1 = pv_names.tails.Bunch_bank.("bank1");
+detectors = pv_names.tails.Detector;
+
 detect_bunch = drive_bunch;
 
 % Only sweep selected bunch
@@ -19,39 +24,39 @@ detect_wf_3 = zeros(1,936);
 detect_wf_3(sort(mod(detect_bunch + 2,936)+1)) = 1;
 
 % write to the TMBF on which bunches to perform the sweep
-lcaPut(['SR23C-DI-TMBF-01:' axis ':BUN:1:SEQ:ENABLE_S'], drive_wf);
+lcaPut([head, bank1.SEQ.enablewf], drive_wf);
 
 % Configure FIR enable (whether feedback is on or off)
-lcaPut(['SR23C-DI-TMBF-01:' axis ':BUN:1:FIR:ENABLE_S'], fb_on_off * ones(1,936));
+lcaPut([head bank1.FIR.enablewf], fb_on_off * ones(1,936));
 
 % Detector 0 grabs everything
 if d0flag
-    lcaPut(['SR23C-DI-TMBF-01:' axis ':DET:0:BUNCHES_S'], ones(1,936));
-    lcaPut(['SR23C-DI-TMBF-01:' axis ':DET:0:ENABLE_S'], 'Enabled');
+    lcaPut([head, detectors.("det0").bunch_selection], ones(1,936));
+    lcaPut([head, detectors.("det0").enable], 'Enabled');
 else
-    lcaPut(['SR23C-DI-TMBF-01:' axis ':DET:0:ENABLE_S'], 'Disabled');
+    lcaPut([head, detectors.("det0").enable], 'Disabled');
 end
 
 % Detector 1 only observes active bunches
 if d1flag
-    lcaPut(['SR23C-DI-TMBF-01:' axis ':DET:1:BUNCHES_S'], detect_wf_1);
-    lcaPut(['SR23C-DI-TMBF-01:' axis ':DET:1:ENABLE_S'], 'Enabled');
+    lcaPut([head, detectors.("det1").bunch_selection], detect_wf_1);
+    lcaPut([head, detectors.("det1").enable], 'Enabled');
 else
-    lcaPut(['SR23C-DI-TMBF-01:' axis ':DET:1:ENABLE_S'], 'Disabled');
+    lcaPut([head, detectors.("det1").enable], 'Disabled');
 end
 
 % Detector 2 only observes 1 bunch behind driven bunches
 if d2flag
-    lcaPut(['SR23C-DI-TMBF-01:' axis ':DET:2:BUNCHES_S'], detect_wf_2);
-    lcaPut(['SR23C-DI-TMBF-01:' axis ':DET:2:ENABLE_S'], 'Enabled');
+    lcaPut([head, detectors.("det2").bunch_selection], detect_wf_2);
+    lcaPut([head, detectors.("det2").enable], 'Enabled');
 else
-    lcaPut(['SR23C-DI-TMBF-01:' axis ':DET:2:ENABLE_S'], 'Disabled');
+    lcaPut([head, detectors.("det2").enable], 'Disabled');
 end
 
 % Detector 3 only observes 2 bunches behind driven bunches
 if d3flag
-    lcaPut(['SR23C-DI-TMBF-01:' axis ':DET:3:BUNCHES_S'], detect_wf_3);
-    lcaPut(['SR23C-DI-TMBF-01:' axis ':DET:3:ENABLE_S'], 'Enabled');
+    lcaPut([head, detectors.("det3").bunch_selection], detect_wf_3);
+    lcaPut([head, detectors.("det3").enable], 'Enabled');
 else
-    lcaPut(['SR23C-DI-TMBF-01:' axis ':DET:3:ENABLE_S'], 'Disabled');
+    lcaPut([head, detectors.("det3").enable], 'Disabled');
 end
