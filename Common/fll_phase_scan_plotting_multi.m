59c19b1e926678f01c06d1b68e66ef66f94a325b
Alun Morgan <alun.morgan@diamond.ac.uk>
2023-11-17 16:17:30 +0000

    Sorting the data so the scan labels show in increasing value order.

--- Common/fll_phase_scan_plotting_multi.m
+++ Common/fll_phase_scan_plotting_multi.m
@@ -16,27 +16,49 @@ addParameter(p, 'scan_type', default_scan_type);
 
 parse(p, requested_data, varargin{:});
 scan_type = p.Results.scan_type;
-% target_bunch = 920;
-% gains = [-30,-24,-18,-12,-6];
-f1 = figure('Position', [50, 100, 1440, 768]);
+
 for ehs = 1:length(requested_data)
-    % IF more than one bunch is selected then only the first one is used here.
-    target_bunch = find(requested_data{ehs}.mbf_fll.target_bunches >0, 1, 'first');
-    data_length = length(requested_data{ehs}.phase);
-    subplot(2,3,1)
+    if strcmp(scan_type, 'samples')
+        xval(ehs) = ehs;
+    elseif strcmp(scan_type, 'charge')
+        % IF more than one bunch is selected then only the first one is used here.
+        %     target_bunch = find(requested_data{ehs}.mbf.(selected_axis).fll.target_bunches >0, 1, 'first');
+        target_bunch = 920; %TEMP
+        xval(ehs) = requested_data{ehs}.fill_pattern(target_bunch) * 1E3;
+    elseif strcmp(scan_type, 'gain')
+        selected_axis = requested_data{ehs}.ax_label;
+        xval(ehs) = requested_data{ehs}.mbf.(selected_axis).fll.nco.gain;
+    elseif strcmp(scan_type, 'rf')
+        xval(ehs) = requested_data{ehs}.RF;
+    end %if
+end %for
+[xval, sorting] = sort(xval);
+
+% gains = [-30,-24,-18,-12,-6];
+f1 = figure('Position', [50, 100, 1900, 768]);
+for ehs = 1:length(requested_data)
+temp_data = requested_data{sorting(ehs)};
+    data_length = length(temp_data.phase);
+    subplot(2,4,1)
     hold all
-    plot(requested_data{ehs}.phase, requested_data{ehs}.mag)
+    plot(temp_data.phase, temp_data.mag, '*')
     xlabel('target phase')
-    ylabel('FLL detected magnitude')
+    ylabel('Loop detected magnitude')
     grid on
-    subplot(2,3,2)
+    subplot(2,4,2)
     hold all
-    plot(requested_data{ehs}.phase, requested_data{ehs}.f)
-    xlabel('target phase')
-    ylabel('FLL detected frequency')
+    plot(temp_data.f, temp_data.phase, '*')
+    xlabel('Loop detected frequency')
+    ylabel('target phase')
     title(['Phase scan of frequency locked loop in ',requested_data{1}.ax_label,' with ', scan_type])
     grid on
-    subplot(2,3,3)
+    subplot(2,4,3)
+    hold all
+    plot(temp_data.f, temp_data.mag, '*')
+    xlabel('Loop detected frequency')
+    ylabel('Loop detected magnitude')
+    grid on
+    subplot(2,4,4)
     hold all
     plot(requested_data{ehs}.iq, '*')
     xlabel('i')
@@ -44,42 +66,31 @@ for ehs = 1:length(requested_data)
     axis equal
     grid on
     
-    if strcmp(scan_type, 'samples')
-        xval = ehs;
-    elseif strcmp(scan_type, 'charge')
-        if ehs >length(requested_data) -3
-            target_bunch = 926;
-        end %if
-        xval = requested_data{ehs}.fill_pattern(target_bunch);
-    elseif strcmp(scan_type, 'gain')
-        xval = requested_data{ehs}.mbf_fll.gain;
-    elseif strcmp(scan_type, 'rf')
-        xval = requested_data{ehs}.RF;
-    end %if
-    plot_X = ones(data_length,1) * xval;
+    plot_X = ones(data_length,1) * xval(ehs);
     plot_Y = requested_data{ehs}.phase;
     mag_Z = requested_data{ehs}.mag;
     f_Z = requested_data{ehs}.f;
     
-    subplot(2,3,4)
+    subplot(2,4,5)
     hold all
     plot3(plot_X, plot_Y, mag_Z)
     ylabel('target phase')
     xlabel(scan_type)
-    zlabel('FLL detected magnitude')
+    zlabel('Loop detected magnitude')
     grid on
     view(3)
     axis ij
     
-    subplot(2,3,5)
+    subplot(2,4,6:7)
     hold all
-    plot3(plot_X, plot_Y, f_Z, 'DisplayName', num2str(xval))
-    ylabel('target phase')
+    plot3(plot_X, f_Z, plot_Y, 'DisplayName', num2str(xval(ehs)))
+    ylabel('Loop detected frequency')
     xlabel(scan_type)
-    zlabel('FLL detected frequency')
+    zlabel('target phase')
     grid on
     view(3)
     axis ij
+    legend
 
     
 end %for
