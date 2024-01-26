aa914c2662668c259c5490580eff2a53cbe30d31
Alun Morgan <alun.morgan@diamond.ac.uk>
2024-01-26 10:50:19 +0000

    Abstracted some hard coded values.

--- Operational_codes/Growdamp/drive_damp_analysis_longitudinal_expfit.m
+++ Operational_codes/Growdamp/drive_damp_analysis_longitudinal_expfit.m
@@ -1,8 +1,11 @@
-function name=drive_damp_analysis_longitudinal_expfit(name)
+function name = drive_damp_analysis_longitudinal_expfit(name)
 % Exponential fit function for drive-damp measurements
 % Last edit: 28.09.2023 (G.Rehm, A.Matveenko)
 
-aa = reshape(abs(name.iq), name.dampcapture, 400);
+
+n_cycles = 400;
+
+aa = reshape(abs(name.iq), name.dampcapture, n_cycles);
 time = (1:name.dampcapture) * name.dampdwell / 1.249e6; % s
 
 % fit parameters are amplitude - p(1), damping - p(2), and "zero"-point offset - p(3)
@@ -11,10 +14,10 @@ f = @(p, time) (p(1) .* exp(-p(2) .* time) + p(3));
 % further "optimisation" of opts can make the fit faster...
 opts = optimset('MaxFunEvals', 50000, 'MaxIter', 10000);
 
-for m = 1:400
+for m = 1:n_cycles
     OLS = @(p) sum((f(p, time) - aa(:,m).').^2); % sum of squares of model-measurement
-    res0 = [aa(1,m);150;aa(300,m)]; % guess-values 
-    p(m,1:3) = fminsearch(OLS,res0,opts);
+    res0 = [aa(1,m); 150; aa(300,m)]; % guess-values 
+    p(m,1:3) = fminsearch(OLS, res0, opts);
     %[p(m,1:2),stemp]=polyfit(t,log(aa(:,m)),1);
     % stemp is a structure, but we only want to keep the regression coefficient
     % so let's extract that and save in s
