4690396a52c6ce1c68148ec75a2a50cf72105197
Alun Morgan <alun.morgan@diamond.ac.uk>
2024-04-29 18:34:48 +0100

    Updated to use a list of BPM names also retrys data aquisition 10 times.

--- Developmental_codes/other_systems/BPM_FR_get_data_XY_only.m
+++ Developmental_codes/other_systems/BPM_FR_get_data_XY_only.m
@@ -1,14 +1,15 @@
-function output = BPM_FR_get_data_XY_only(nbpms)
+function output = BPM_FR_get_data_XY_only(bpm_list)
 % Captures 2048 turns of data
 
-for n = 1:length(nbpms)
-    bpm_name = fa_id2name(nbpms(n));
+for n = 1:length(bpm_list)
+    bpm_name = bpm_list{n};
+    bpm_label = regexprep(bpm_name, '-', '_');
     for tk = 1:10
-        bpm_label = regexprep(bpm_name, '-', '_');
         try
             BPM_data_temp = get_variable({[bpm_name, ':FR:WFX'];[bpm_name, ':FR:WFY'];});
             output.(bpm_label).X = BPM_data_temp(1, :);
             output.(bpm_label).Y = BPM_data_temp(2, :);
+            fprintf('.')
             break
         catch
             disp(['Failed to get BPM Free run data for ', bpm_name])
--- Developmental_codes/other_systems/BPM_FT_get_data_XY_only.m
+++ Developmental_codes/other_systems/BPM_FT_get_data_XY_only.m
@@ -1,18 +1,23 @@
-function output = BPM_FT_get_data_XY_only(nbpms)
+function output = BPM_FT_get_data_XY_only(bpm_list)
 
 
-BPM_FT_data = NaN(length(nbpms), 2);
-for n = 1:length(nbpms)
-    BPM_name = fa_id2name(nbpms(n));
-    BPM_data_temp = get_variable({...
-        [BPM_name, ':FT:WFX'];...
-        [BPM_name, ':FT:WFY'];...
-        });
-    BPM_FT_data(n, 1:size(BPM_data_temp,1), 1:size(BPM_data_temp,2)) = BPM_data_temp;
+BPM_FT_data = NaN(length(bpm_list), 2);
+for n = 1:length(bpm_list)
+    bpm_name = bpm_list{n};
+    bpm_label = regexprep(bpm_name, '-', '_');
+    for tk = 1:10
+        try
+            BPM_data_temp = get_variable({...
+                [bpm_name, ':FT:WFX'];...
+                [bpm_name, ':FT:WFY'];...
+                });
+            BPM_FT_data(n, 1:size(BPM_data_temp,1), 1:size(BPM_data_temp,2)) = BPM_data_temp;
+            output.(bpm_label).X = squeeze(BPM_data_temp(1, :));
+            output.(bpm_label).Y = squeeze(BPM_data_temp(2, :));
+            fprintf('.')
+            break
+        catch
+            disp(['Failed to get BPM first turn data for ', bpm_name])
+        end %try
+    end %for
 end %for
-
-for n=1:length(nbpms)
-    bpm_name = regexprep(fa_id2name(nbpms(n)), '-', '_');
-    output.(bpm_name).X = squeeze(BPM_FT_data(n, 1, :));
-    output.(bpm_name).Y = squeeze(BPM_FT_data(n, 2, :));
-end
