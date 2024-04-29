24cbdf6167e1cd73797ded6c83f393066f007d37
Alun Morgan <alun.morgan@diamond.ac.uk>
2024-04-29 18:26:22 +0100

    Now uses a list of BPM names which is more flexible. Will default to XY data only but have flags to add additional data to the capture.

--- Developmental_codes/other_systems/BPM_SA_get_data.m
+++ Developmental_codes/other_systems/BPM_SA_get_data.m
@@ -1,34 +1,42 @@
-function output = BPM_SA_get_data
+function output = BPM_SA_get_data(bpm_list, varargin)
+% Defaults to X and Y waveforms, ABCD, Q and current can be added
+% with the flag 'all_channels'
 
-n_samples = 30;
-n_bpms = 173;
+p = inputParser;
+p.StructExpand = false;
+p.CaseSensitive = false;
+boolean_string = {'yes', 'no'};
 
-BPM_SA_data = NaN(n_bpms, 8, n_samples);
-for jsp = 1:n_samples
-    for n=1:n_bpms
-        BPM_name = fa_id2name(n);
-        BPM_data_temp = get_variable({[BPM_name, ':SA:X']; ...
-            [BPM_name, ':SA:Y'];...
-            [BPM_name, ':SA:Q'];...
-            [BPM_name, ':SA:CURRENT'];...
-            [BPM_name, ':SA:A'];...
-            [BPM_name, ':SA:B'];...
-            [BPM_name, ':SA:C'];...
-            [BPM_name, ':SA:D']});
-        
-        BPM_SA_data(n, 1:size(BPM_data_temp,1),jsp) = BPM_data_temp;
+addRequired(p, 'bpm_list');
+addRequired(p, 'capture_length');
+addParameter(p, 'n_samples', 30)
+addParameter(p, 'all_channels', 'no', @(x) any(validatestring(x, boolean_string)));
+
+parse(p, nbpms, varargin{:});
+
+n_bpms = length(bpm_list);
+
+for n = n_bpms:-1:1
+    for jsp = n_samples:-1:1
+        bpm_name = bpm_list{n};
+        bpm_label = regexprep(bpm_name, '-', '_');
+        BPM_data_temp = get_variable({[bpm_name, ':SA:X']; ...
+            [bpm_name, ':SA:Y'];});
+        output.(bpm_label).X = squeeze(BPM_data_temp(1, jsp));
+        output.(bpm_label).Y = squeeze(BPM_data_temp(2, jsp));
+        if strcmp(p.Results.all_channels, 'yes')
+            BPM_data_temp_add = get_variable({[bpm_name, ':SA:Q'];...
+                [bpm_name, ':SA:CURRENT'];...
+                [bpm_name, ':SA:A'];...
+                [bpm_name, ':SA:B'];...
+                [bpm_name, ':SA:C'];...
+                [bpm_name, ':SA:D']});
+            output.(bpm_label).Q = squeeze(BPM_data_temp_add(1, jsp));
+            output.(bpm_label).CURRENT = squeeze(BPM_data_temp_add(2, jsp));
+            output.(bpm_label).A = squeeze(BPM_data_temp_add(3, jsp));
+            output.(bpm_label).B = squeeze(BPM_data_temp_add(4, jsp));
+            output.(bpm_label).C = squeeze(BPM_data_temp_add(5, jsp));
+            output.(bpm_label).D = squeeze(BPM_data_temp_add(6, jsp));
+        end %if
     end %for
 end %for
-
-for n=1:n_bpms
-    bpm_name = regexprep(fa_id2name(n), '-', '_');
-output.(bpm_name).X = squeeze(BPM_SA_data(n, 1, :));
-output.(bpm_name).Y = squeeze(BPM_SA_data(n, 2, :));
-output.(bpm_name).Q = squeeze(BPM_SA_data(n, 3, :));
-output.(bpm_name).CURRENT = squeeze(BPM_SA_data(n, 4, :));
-output.(bpm_name).A = squeeze(BPM_SA_data(n, 5, :));
-output.(bpm_name).B = squeeze(BPM_SA_data(n, 6, :));
-output.(bpm_name).C = squeeze(BPM_SA_data(n, 7, :));
-output.(bpm_name).D = squeeze(BPM_SA_data(n, 8, :));
-end
-
--- Developmental_codes/other_systems/BPM_TbT_get_data.m
+++ Developmental_codes/other_systems/BPM_TbT_get_data.m
@@ -12,7 +12,6 @@ addRequired(p, 'capture_length');
 addParameter(p, 'all_waveforms', 'no', @(x) any(validatestring(x, boolean_string)));
 addParameter(p, 'capture_stats', 'no', @(x) any(validatestring(x, boolean_string)));
 
-% parse(p, mbf_axis);
 parse(p, nbpms, capture_length,  varargin{:});
 
 for n = 1:length(bpm_list)
@@ -23,25 +22,30 @@ for n = 1:length(bpm_list)
             BPM_data_temp = get_variable({[bpm_name, ':TT:WFX'];[bpm_name, ':TT:WFY'];});
             output.(bpm_label).X = BPM_data_temp(1, 1:capture_length);
             output.(bpm_label).Y = BPM_data_temp(2, 1:capture_length);
-            BPM_data_temp2 = get_variable({[BPM_name, ':TT:WFS'];...
-                [BPM_name, ':TT:WFA'];...
-                [BPM_name, ':TT:WFB'];...
-                [BPM_name, ':TT:WFC'];...
-                [BPM_name, ':TT:WFD']});
-            output.(bpm_label).intensity = BPM_data_temp2(1, 1:capture_length);
-            output.(bpm_label).A = BPM_data_temp2(2, 1:capture_length);
-            output.(bpm_label).B = BPM_data_temp2(3, 1:capture_length);
-            output.(bpm_label).C = BPM_data_temp2(4, 1:capture_length);
-            output.(bpm_label).D = BPM_data_temp2(5, 1:capture_length);
+            if strcmp(p.Results.all_waveforms, 'yes')
+                BPM_data_temp2 = get_variable({[BPM_name, ':TT:WFS'];...
+                    [BPM_name, ':TT:WFA'];...
+                    [BPM_name, ':TT:WFB'];...
+                    [BPM_name, ':TT:WFC'];...
+                    [BPM_name, ':TT:WFD']});
+                output.(bpm_label).intensity = BPM_data_temp2(1, 1:capture_length);
+                output.(bpm_label).A = BPM_data_temp2(2, 1:capture_length);
+                output.(bpm_label).B = BPM_data_temp2(3, 1:capture_length);
+                output.(bpm_label).C = BPM_data_temp2(4, 1:capture_length);
+                output.(bpm_label).D = BPM_data_temp2(5, 1:capture_length);
+            end %if
             % Statistics of waveforms
-            output.(bpm_label).meanx = lcaGet([bpm, ':TT:MEANX']);
-            output.(bpm_label).meany = lcaGet([bpm, ':TT:MEANY']);
-            output.(bpm_label).stdx = lcaGet([bpm, ':TT:STDX']);
-            output.(bpm_label).stdy = lcaGet([bpm, ':TT:STDY']);
-            output.(bpm_label).minx = lcaGet([bpm, ':TT:MINX']);
-            output.(bpm_label).miny = lcaGet([bpm, ':TT:MINY']);
-            output.(bpm_label).maxx = lcaGet([bpm, ':TT:MAXX']);
-            output.(bpm_label).maxy = lcaGet([bpm, ':TT:MAXY']);
+            if strcmp(p.Results.capture_stats, 'yes')
+                output.(bpm_label).meanx = lcaGet([bpm, ':TT:MEANX']);
+                output.(bpm_label).meany = lcaGet([bpm, ':TT:MEANY']);
+                output.(bpm_label).stdx = lcaGet([bpm, ':TT:STDX']);
+                output.(bpm_label).stdy = lcaGet([bpm, ':TT:STDY']);
+                output.(bpm_label).minx = lcaGet([bpm, ':TT:MINX']);
+                output.(bpm_label).miny = lcaGet([bpm, ':TT:MINY']);
+                output.(bpm_label).maxx = lcaGet([bpm, ':TT:MAXX']);
+                output.(bpm_label).maxy = lcaGet([bpm, ':TT:MAXY']);
+            end %if
+            fprintf('.')
             break
         catch
             disp(['Failed to get BPM TbT data for ', bpm_name])
