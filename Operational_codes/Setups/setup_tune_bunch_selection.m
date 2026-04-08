36953b254592c9f91750dad232ac8d7ffd63f891
Alun Morgan <alun.morgan@diamond.ac.uk>
2026-04-08 16:45:00 +0100

    Removed hard coded values. Removed gain setting. This only sets the pattern.

--- Operational_codes/Setups/setup_tune_bunch_selection.m
+++ Operational_codes/Setups/setup_tune_bunch_selection.m
@@ -1,38 +1,30 @@
-function setup_tune_bunch_selection(mbf_axis, tune_mode)
+function setup_tune_bunch_selection(mbf_axis)
 % This allows the setup of the tune to be adjusted from using all bunches to
 % using a subset. This is useful if the tune measurement is struggling due to
-% distortion of the IQ plot.
+% distortion of the IQ plot. Defaults to all bunches.
 %
-% Example: setup_tune_bunch_selection('x', 'comb')
+% Example: setup_tune_bunch_selection('x', 40, 90, 10)
 
-PV_base = 'SR23C-DI-TMBF-01:';
-detector0 =  ':DET:0:BUNCHES_S';
-sequencer1 = ':BUN:1:SEQ:ENABLE_S';
-excitation = ':SEQ:1:GAIN_DB_S';
+
+[~, harmonic_number, pv_names, ~] = mbf_system_config;
+pv_head = pv_names.hardware_names.(mbf_axis);
+detector0 =  pv_names.tails.Detector.det0.bunch_selection;
+sequencer1 = pv_names.tails.Bunch_bank.bank1.SEQ.enablewf;
 
 expectedAxes = {'x','X', 'y', 'Y'};
-expectedModes = {'all', 'comb'};
+valid_positive_number = @(x) isnumeric(x) && isscalar(x) && (x >= 0);
+
 p = inputParser;
 addRequired(p,'mbf_axis',@(x) any(validatestring(x,expectedAxes)));
-addRequired(p,'tune_mode',@(x) any(validatestring(x,expectedModes)));
+addParameter(p,'start_bunch', 1, valid_positive_number);
+addParameter(p,'step_bunch', 1, valid_positive_number);
+addParameter(p,'n_bunch', harmonic_number, valid_positive_number);
+
 parse(p, mbf_axis, tune_mode);
 
-selected_axis = upper(p.Results.mbf_axis);
-
-if strcmpi(p.Results.tune_mode, 'comb')
-    % setup tune using 10 bunches spaced 90 bucket apart. Starting at
-    % bucket 40.
-    det_enable_wfm = zeros(936,1);
-    for js = 1:10
-        det_enable_wfm(js * 90 - 50) = 1;
-    end %for
-    lcaPut([PV_base, selected_axis, detector0 ], det_enable_wfm')
-    lcaPut([PV_base, selected_axis, sequencer1], det_enable_wfm')
-    lcaPut([PV_base, selected_axis, excitation], -36.12)
-elseif strcmpi(p.Results.tune_mode, 'all')
-    % revert back to using all bunches.
-    det_enable_wfm = ones(936,1);
-    lcaPut([PV_base, selected_axis, detector0], det_enable_wfm')
-    lcaPut([PV_base, selected_axis, sequencer1], det_enable_wfm')
-    lcaPut([PV_base, selected_axis, excitation], -54.19)
-end %if
+det_enable_wfm = zeros(harmonic_number,1);
+for js = 1:p.Results.n_bunch
+    det_enable_wfm(p.Results.start_bunch + (js - 1) * p.Results.step_bunch) = 1;
+end %for
+lcaPut([pv_head, detector0 ], det_enable_wfm')
+lcaPut([pv_head, sequencer1], det_enable_wfm')
