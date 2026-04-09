8cc3f07a75a74825e09e70c7fd7a77de8086a5ce
Alun Morgan <alun.morgan@diamond.ac.uk>
2026-04-09 10:16:32 +0100

    Commented out (hopfully) unused code prior to testing. Moved some sections to better distinguish between MBF and external systems.

--- Common/mbf_system_config.m
+++ Common/mbf_system_config.m
@@ -2,8 +2,7 @@ function [root_path, harmonic_number, pv_names, trigger_inputs] = mbf_system_con
 
 %       harmonic_number (int): Harmonic number of the machine.
 
-% The first entry in root_path is the currently used location for capture.
-% Additional locations are for alternative archival locations only.
+% root_path is the location for captured data.
 root_path = '/dls/ops-data/Diagnostics/MBF/';
 harmonic_number = 936;
 
@@ -19,6 +18,30 @@ pv_names.bunch_pattern2 = 'SR-DI-PICO-02:BUCKETS_180';
 
 %% Topup countdown
 pv_names.topup.countdown = 'SR-CS-FILL-01:COUNTDOWN';
+
+%% Pinhole PVs
+pv_names.pinhole1 = 'SR01C-DI-DCAM-04';
+pv_names.pinhole2 = 'SR01C-DI-DCAM-05';
+
+%% Emittance measurement
+pv_names.emittance.status = 'SR-DI-EMIT-01:STATUS';
+pv_names.emittance.x.val = 'SR-DI-EMIT-01:HEMIT';
+pv_names.emittance.x.mean = 'SR-DI-EMIT-01:HEMIT_MEAN';
+pv_names.emittance.x.error = 'SR-DI-EMIT-01:HERROR';
+pv_names.energy_spread.val = 'SR-DI-EMIT-01:ESPREAD';
+pv_names.energy_spread.mean = 'SR-DI-EMIT-01:X:ESPREAD_MEAN';
+pv_names.emittance.y.val = 'SR-DI-EMIT-01:VEMIT';
+pv_names.emittance.y.mean = 'SR-DI-EMIT-01:VEMIT_MEAN';
+pv_names.emittance.y.error = 'SR-DI-EMIT-01:VERROR';
+pv_names.coupling.val = 'SR-DI-EMIT-01:COUPLING';
+pv_names.coupling.mean = 'SR-DI-EMIT-01:COUPLING_MEAN';
+pv_names.beam_size.pinhole1.sigmax = 'SR-DI-EMIT-01:P1:SIGMAX';
+pv_names.beam_size.pinhole1.sigmay = 'SR-DI-EMIT-01:P1:SIGMAY';
+pv_names.beam_size.pinhole2.sigmax = 'SR-DI-EMIT-01:P2:SIGMAX';
+pv_names.beam_size.pinhole2.sigmay = 'SR-DI-EMIT-01:P2:SIGMAY';
+
+%% External trigger
+pv_names.Hardware_trigger = 'LI-TI-MTGEN-01:BS-DI-MODE';
 
 %% DORIS PVs
 pv_names.doris.phase = 'SR23C-DI-DORIS-01:TARGET_S';
@@ -48,30 +71,6 @@ pv_names.hardware_names.mem.tx = pv_names.hardware_names.lab; % test system
 pv_names.hardware_names.mem.ty = pv_names.hardware_names.lab; % test system
 pv_names.hardware_names.tx = [pv_names.hardware_names.lab, ':X']; % test system
 pv_names.hardware_names.ty = [pv_names.hardware_names.lab, ':Y']; % test system
-
-%% External trigger
-pv_names.Hardware_trigger = 'LI-TI-MTGEN-01:BS-DI-MODE';
-
-%% Pinhole PVs
-pv_names.pinhole1 = 'SR01C-DI-DCAM-04';
-pv_names.pinhole2 = 'SR01C-DI-DCAM-05';
-
-%% Emittance measurement
-pv_names.emittance.status = 'SR-DI-EMIT-01:STATUS';
-pv_names.emittance.x.val = 'SR-DI-EMIT-01:HEMIT';
-pv_names.emittance.x.mean = 'SR-DI-EMIT-01:HEMIT_MEAN';
-pv_names.emittance.x.error = 'SR-DI-EMIT-01:HERROR';
-pv_names.energy_spread.val = 'SR-DI-EMIT-01:ESPREAD';
-pv_names.energy_spread.mean = 'SR-DI-EMIT-01:X:ESPREAD_MEAN';
-pv_names.emittance.y.val = 'SR-DI-EMIT-01:VEMIT';
-pv_names.emittance.y.mean = 'SR-DI-EMIT-01:VEMIT_MEAN';
-pv_names.emittance.y.error = 'SR-DI-EMIT-01:VERROR';
-pv_names.coupling.val = 'SR-DI-EMIT-01:COUPLING';
-pv_names.coupling.mean = 'SR-DI-EMIT-01:COUPLING_MEAN';
-pv_names.beam_size.pinhole1.sigmax = 'SR-DI-EMIT-01:P1:SIGMAX';
-pv_names.beam_size.pinhole1.sigmay = 'SR-DI-EMIT-01:P1:SIGMAY';
-pv_names.beam_size.pinhole2.sigmax = 'SR-DI-EMIT-01:P2:SIGMAX';
-pv_names.beam_size.pinhole2.sigmay = 'SR-DI-EMIT-01:P2:SIGMAY';
 
 %% Trigger settings
 pv_names.tails.triggers.mode = ':TRG:SEQ:MODE_S';
@@ -107,23 +106,20 @@ pv_names.tails.Super_sequencer.count = ':SEQ:SUPER:COUNT_S';
 pv_names.tails.Super_sequencer.reset = ':SEQ:SUPER:RESET_S.PROC';
 
 %% Sequencer settings
-pv_names.tails.Sequencer.start_state = ':SEQ:PC_S';
-pv_names.tails.Sequencer.steady_state_bank = ':SEQ:0:BANK_S';
-pv_names.tails.Sequencer.Base = ':SEQ';
-pv_names.tails.Sequencer.start_frequency = ':START_FREQ_S';
-pv_names.tails.Sequencer.end_frequency = ':END_FREQ_S';
-pv_names.tails.Sequencer.count = ':COUNT_S';
-pv_names.tails.Sequencer.dwell = ':DWELL_S';
-pv_names.tails.Sequencer.gain = ':GAIN_S';
-pv_names.tails.Sequencer.gaindb = ':GAIN_DB_S';
-pv_names.tails.Sequencer.enable = ':ENABLE_S';
-pv_names.tails.Sequencer.step_frequency = ':STEP_FREQ_S';
-pv_names.tails.Sequencer.holdoff = ':HOLDOFF_S';
-pv_names.tails.Sequencer.bank_select = ':BANK_S';
-pv_names.tails.Sequencer.capture_state = ':CAPTURE_S';
-pv_names.tails.Sequencer.windowing_state = ':ENWIN_S';
-pv_names.tails.Sequencer.blanking_state = ':BLANK_S';
-pv_names.tails.Sequencer.reset = ':RESET_S.PROC';
+% pv_names.tails.Sequencer.Base = ':SEQ';
+% pv_names.tails.Sequencer.start_frequency = ':START_FREQ_S';
+% pv_names.tails.Sequencer.end_frequency = ':END_FREQ_S';
+% pv_names.tails.Sequencer.count = ':COUNT_S';
+% pv_names.tails.Sequencer.dwell = ':DWELL_S';
+% pv_names.tails.Sequencer.gain = ':GAIN_S';
+% pv_names.tails.Sequencer.gaindb = ':GAIN_DB_S';
+% pv_names.tails.Sequencer.enable = ':ENABLE_S';
+% pv_names.tails.Sequencer.step_frequency = ':STEP_FREQ_S';
+% pv_names.tails.Sequencer.holdoff = ':HOLDOFF_S';
+% pv_names.tails.Sequencer.bank_select = ':BANK_S';
+% pv_names.tails.Sequencer.capture_state = ':CAPTURE_S';
+% pv_names.tails.Sequencer.windowing_state = ':ENWIN_S';
+% pv_names.tails.Sequencer.blanking_state = ':BLANK_S';
 
 % applies to all
 pv_names.tails.Sequencer.start_state = ':SEQ:PC_S';
@@ -131,8 +127,8 @@ pv_names.tails.Sequencer.steady_state_bank = ':SEQ:0:BANK_S';
 pv_names.tails.Sequencer.reset = ':SEQ:RESET_S.PROC';
 
 for n_seq = 1:7
-    l_seq = ['seq',num2str(n_seq)];
     n_seq_label = num2str(n_seq);
+    l_seq = ['seq', n_seq_label];
     pv_names.tails.Sequencer.(l_seq).start_frequency = [':SEQ:', n_seq_label,':START_FREQ_S'];
     pv_names.tails.Sequencer.(l_seq).end_frequency = [':SEQ:', n_seq_label,':END_FREQ_S'];
     pv_names.tails.Sequencer.(l_seq).turns = [':SEQ:', n_seq_label, ':COUNT_S'];
@@ -151,46 +147,45 @@ for n_seq = 1:7
 
 end %for
 
-
 %% Bank settings
-pv_names.tails.Bunch_bank.Base = ':BUN';
-pv_names.tails.Bunch_bank.Gains = ':GAINWF_S';
-pv_names.tails.Bunch_bank.Output_types = ':OUTWF_S';
-pv_names.tails.Bunch_bank.FIR_select = ':FIRWF_S';
-pv_names.tails.Bunch_bank.FIR_set_enable = ':FIR:SET_ENABLE_S.PROC';
-pv_names.tails.Bunch_bank.FIR_enable = ':FIR:ENABLE_S';
-pv_names.tails.Bunch_bank.NCO1_set_enable = ':NCO1:SET_ENABLE_S.PROC';
-pv_names.tails.Bunch_bank.NCO2_set_enable = ':NCO2:SET_ENABLE_S.PROC';
-pv_names.tails.Bunch_bank.NCO1_enable = ':NCO1:ENABLE_S';
-pv_names.tails.Bunch_bank.NCO2_enable = ':NCO2:ENABLE_S';
-pv_names.tails.Bunch_bank.SEQ_set_enable = ':SEQ:SET_ENABLE_S.PROC';
-pv_names.tails.Bunch_bank.SEQ_enable = ':SEQ:ENABLE_S';
-pv_names.tails.Bunch_bank.PLL_enable = ':PLL:SET_ENABLE_S';
-pv_names.tails.Bunch_bank.FIR_set_disable = ':FIR:SET_DISABLE_S.PROC';
-pv_names.tails.Bunch_bank.FIR_disable = ':FIR:DISABLE_S';
-pv_names.tails.Bunch_bank.NCO1_disable = ':NCO1:SET_DISABLE_S.PROC';
-pv_names.tails.Bunch_bank.NCO2_disable = ':NCO2:SET_DISABLE_S.PROC';
-pv_names.tails.Bunch_bank.SEQ_disable = ':SEQ:SET_DISABLE_S.PROC';
-pv_names.tails.Bunch_bank.PLL_disable = ':PLL:SET_DISABLE_S.PROC';
-pv_names.tails.Bunch_bank.FIR_gains = ':FIR:GAIN_S';
-pv_names.tails.Bunch_bank.NCO1_gains = ':NCO1:GAIN_S';
-pv_names.tails.Bunch_bank.NCO2_gains = ':NCO2:GAIN_S';
-pv_names.tails.Bunch_bank.SEQ_gains = ':SEQ:GAIN_S';
-pv_names.tails.Bunch_bank.PLL_gains = ':PLL:GAIN_S';
+% pv_names.tails.Bunch_bank.Base = ':BUN';
+% pv_names.tails.Bunch_bank.Gains = ':GAINWF_S';
+% pv_names.tails.Bunch_bank.Output_types = ':OUTWF_S';
+% pv_names.tails.Bunch_bank.FIR_select = ':FIRWF_S';
+% pv_names.tails.Bunch_bank.FIR_set_enable = ':FIR:SET_ENABLE_S.PROC';
+% pv_names.tails.Bunch_bank.FIR_enable = ':FIR:ENABLE_S';
+% pv_names.tails.Bunch_bank.NCO1_set_enable = ':NCO1:SET_ENABLE_S.PROC';
+% pv_names.tails.Bunch_bank.NCO2_set_enable = ':NCO2:SET_ENABLE_S.PROC';
+% pv_names.tails.Bunch_bank.NCO1_enable = ':NCO1:ENABLE_S';
+% pv_names.tails.Bunch_bank.NCO2_enable = ':NCO2:ENABLE_S';
+% pv_names.tails.Bunch_bank.SEQ_set_enable = ':SEQ:SET_ENABLE_S.PROC';
+% pv_names.tails.Bunch_bank.SEQ_enable = ':SEQ:ENABLE_S';
+% pv_names.tails.Bunch_bank.PLL_enable = ':PLL:SET_ENABLE_S';
+% pv_names.tails.Bunch_bank.FIR_set_disable = ':FIR:SET_DISABLE_S.PROC';
+% pv_names.tails.Bunch_bank.FIR_disable = ':FIR:DISABLE_S';
+% pv_names.tails.Bunch_bank.NCO1_disable = ':NCO1:SET_DISABLE_S.PROC';
+% pv_names.tails.Bunch_bank.NCO2_disable = ':NCO2:SET_DISABLE_S.PROC';
+% pv_names.tails.Bunch_bank.SEQ_disable = ':SEQ:SET_DISABLE_S.PROC';
+% pv_names.tails.Bunch_bank.PLL_disable = ':PLL:SET_DISABLE_S.PROC';
+% pv_names.tails.Bunch_bank.FIR_gains = ':FIR:GAIN_S';
+% pv_names.tails.Bunch_bank.NCO1_gains = ':NCO1:GAIN_S';
+% pv_names.tails.Bunch_bank.NCO2_gains = ':NCO2:GAIN_S';
+% pv_names.tails.Bunch_bank.SEQ_gains = ':SEQ:GAIN_S';
+% pv_names.tails.Bunch_bank.PLL_gains = ':PLL:GAIN_S';
 
 for n_bank = 0:3
-    l_bank = ['bank',num2str(n_bank)];
     n_bank_label = num2str(n_bank);
+    l_bank = ['bank',n_bank_label];
     pv_names.tails.Bunch_bank.(l_bank).Output_types = [':BUN:', n_bank_label, ':OUTWF_S'];
     pv_names.tails.Bunch_bank.(l_bank).bunch_select = [':BUN:', n_bank_label, ':BUNCH_SELECT_S'];
 
-    pv_names.tails.Bunch_bank.(l_bank).FIR.enablewf = [':BUN:', n_bank_label, ':FIR:ENABLE_S']; 
+    pv_names.tails.Bunch_bank.(l_bank).FIR.enablewf = [':BUN:', n_bank_label, ':FIR:ENABLE_S'];
     pv_names.tails.Bunch_bank.(l_bank).FIR.set_enable = [':BUN:', n_bank_label, ':FIR:SET_ENABLE_S.PROC'];
     pv_names.tails.Bunch_bank.(l_bank).FIR.set_disable = [':BUN:', n_bank_label, ':FIR:SET_DISABLE_S.PROC'];
-    pv_names.tails.Bunch_bank.(l_bank).FIR.gainwf = [':BUN:', n_bank_label, ':FIR:GAIN_S']; 
-    pv_names.tails.Bunch_bank.(l_bank).FIR.gainswfdb = [':BUN:', n_bank_label, ':FIR:GAIN_DB']; 
+    pv_names.tails.Bunch_bank.(l_bank).FIR.gainwf = [':BUN:', n_bank_label, ':FIR:GAIN_S'];
+    pv_names.tails.Bunch_bank.(l_bank).FIR.gainswfdb = [':BUN:', n_bank_label, ':FIR:GAIN_DB'];
     %     Select which filter to use on which bunch
-    pv_names.tails.Bunch_bank.(l_bank).FIR.selectwf = [':BUN:', n_bank_label, ':FIRWF_S']; 
+    pv_names.tails.Bunch_bank.(l_bank).FIR.selectwf = [':BUN:', n_bank_label, ':FIRWF_S'];
     pv_names.tails.Bunch_bank.(l_bank).FIR.set_select = [':BUN:', n_bank_label, ':FIRWF_SET_S.PROC'];
 
     pv_names.tails.Bunch_bank.(l_bank).NCO1.enablewf = [':BUN:', n_bank_label, ':NCO1:ENABLE_S'];
@@ -224,8 +219,8 @@ pv_names.tails.Detector.source = ':DET:SELECT_S';
 pv_names.tails.Detector.fill_waveforms = ':DET:FILL_WAVEFORMS_S';
 pv_names.tails.Detector.fir_delay = ':DET:FIR_DELAY_S';
 for n_det = 0:3
-    l_det = ['det',num2str(n_det)];
     n_det_label = num2str(n_det);
+    l_det = ['det', n_det_label];
     pv_names.tails.Detector.(l_det).enable = [':DET:',n_det_label,':ENABLE_S'];
     pv_names.tails.Detector.(l_det).bunch_selection = [':DET:',n_det_label,':BUNCHES_S']; % How do you set all?
     pv_names.tails.Detector.(l_det).scale = [':DET:',n_det_label,':SCALING_S']; % gain?
@@ -246,21 +241,17 @@ pv_names.tails.MEM.channel0_target = ':MEM:SEL0_S'; % NEED TO TAKE OFF THE X|Y B
 pv_names.tails.MEM.channel1_target = ':MEM:SEL1_S'; % NEED TO TAKE OFF THE X|Y BEFORE USE.
 pv_names.tails.TRG.memory_status = ':TRG:MEM:STATUS';
 
-
-%% NCO1 settings
-pv_names.tails.NCO1.frequency = ':NCO1:FREQ_S';
-pv_names.tails.NCO1.gain = ':NCO1:GAIN_S';
-pv_names.tails.NCO1.gaindb = ':NCO1:GAIN_DB_S';
-pv_names.tails.NCO1.enable = ':NCO1:ENABLE_S';
-pv_names.tails.NCO1.PLL_follow = ':NCO1:TUNE_PLL_S';
-
-%% NCO2 settings
-pv_names.tails.NCO2.gain_scalar = ':NCO2:GAIN_SCALAR_S';
-pv_names.tails.NCO2.gain_db = ':NCO2:GAIN_DB_S';
-pv_names.tails.NCO2.enable = ':NCO2:ENABLE_S';
-pv_names.tails.NCO2.frequency = ':NCO2:FREQ_S';
-pv_names.tails.NCO2.enable = ':NCO2:ENABLE_S';
-pv_names.tails.NCO2.PLL_follow = ':NCO2:TUNE_PLL_S';
+%% NCO settings
+for n_nco = 1:2
+    n_nco_label = num2str(n_nco);
+    l_nco = ['NCO',n_nco_label];
+    pv_names.tails.(l_nco).frequency = [':NCO', n_nco_label,':FREQ_S'];
+    pv_names.tails.(l_nco).gain = [':NCO', n_nco_label,':GAIN_S'];
+    pv_names.tails.(l_nco).gain_scalar = [':NCO', n_nco_label,':GAIN_SCALAR_S'];
+    pv_names.tails.(l_nco).gaindb = [':NCO', n_nco_label,':GAIN_DB_S'];
+    pv_names.tails.(l_nco).enable = [':NCO', n_nco_label,':ENABLE_S'];
+    pv_names.tails.(l_nco).PLL_follow = [':NCO', n_nco_label,':TUNE_PLL_S'];
+end %for
 
 %% FIR settings
 pv_names.tails.FIR.gain = ':FIR:GAIN_S';
@@ -319,7 +310,6 @@ pv_names.tails.adc.reject_count = ':ADC:REJECT_COUNT_S';
 pv_names.tails.adc.phase.mean = ':ADC:PHASE_MEAN';
 pv_names.tails.adc.dram_source = ':ADC:DRAM_SOURCE_S';
 pv_names.tails.adc.loopback = ':ADC:LOOPBACK_S';
-
 
 %% DAC buffers
 pv_names.tails.dac.delta = ':DAC:MMS:DELTA';
